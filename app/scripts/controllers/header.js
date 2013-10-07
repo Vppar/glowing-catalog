65af74a26094de28c6019526702f82b070bad54b
dj <dj@192.168.0.109>
2013-10-07 16:38:54 -0300

    added dialogs style end controllers

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('glowingCatalogApp').controller('HeaderCtrl', function($scope, dialog) {
+
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,7 @@
 'use strict';
 
 angular.module('glowingCatalogApp').controller('MainCtrl', function($scope, $dialog, $location) {
-   $scope.awesomeThings = [
-        'HTML5 Boilerplate', 'AngularJS', 'Karma'
-    ];
+
     $scope.openDialogAddToBasket = function() {
 		var d = $dialog.dialog ({ backdropClick: true,
 			dialogClass: 'modal'
@@ -19,4 +17,11 @@ angular.module('glowingCatalogApp').controller('MainCtrl', function($scope, $dia
 	$scope.goToBasket = function() {
 		$location.path('basket');
 	}
+	$scope.openDialogChooseCustomer = function() {
+		var d = $dialog.dialog ({ backdropClick: true,
+			dialogClass: 'modal'
+		});
+		d.open('views/choose-customer-dialog.html', 'ChooseCustomerDialogCtrl');
+	}
+
 });
