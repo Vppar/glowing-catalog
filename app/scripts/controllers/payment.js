90542e2ac0bd517cd567a4e3a8a9f18bd8142c56
dj <dj@192.168.0.109>
2013-10-09 11:59:46 -0300

    - optimized buttons  - linkage views with dialog choose-customer and more css...

--- app/scripts/controllers/payment.js
+++ app/scripts/controllers/payment.js
@@ -21,5 +21,11 @@ angular.module('glowingCatalogApp').controller('PaymentCtrl', function($scope, $
 	};
 	$scope.goToBasket = function() {
 		$location.path('basket');
+	};
+	$scope.openDialogChooseCustomer = function() {
+		var d = $dialog.dialog ({ backdropClick: true,
+			dialogClass: 'modal'
+		});
+		d.open('views/choose-customer-dialog.html', 'ChooseCustomerDialogCtrl');
 	}
 });
