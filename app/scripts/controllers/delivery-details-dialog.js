e9c03ed2e76ee842bcdad49893b05080dde13cc4
dj <dj@192.168.0.111>
2013-10-16 18:00:23 -0300

    added delivery-details dialog

---
+++ app/scripts/controllers/delivery-details-dialog.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('glowingCatalogApp').controller('DeliveryDetailsDialogCtrl', function($scope, dialog) {
+
+
+	$scope.closeDialog = function(){
+		dialog.close();
+	};
+	
+
+});
--- app/scripts/controllers/partial-delivery.js
+++ app/scripts/controllers/partial-delivery.js
@@ -1,9 +1,13 @@
 'use strict';
 
-angular.module('glowingCatalogApp').controller('PartialDeliveryCtrl', function($scope) {
+angular.module('glowingCatalogApp').controller('PartialDeliveryCtrl', function($scope, $dialog) {
 
-
-
-	
+	$scope.openDeliveryDetails = function() {
+		var d = $dialog.dialog({
+			backdropClick : true,
+			dialogClass : 'modal'
+		});
+		d.open('views/delivery-details-dialog.html', 'DeliveryDetailsDialogCtrl');
+	};
 
 });
