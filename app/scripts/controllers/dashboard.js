fbd94e8257d941c9c34df5129cebf04f3c7f9aca
dj <dj@192.168.0.111>
2013-10-24 11:23:37 -0200

    removed empty file

--- app/scripts/controllers/dashboard.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('glowingCatalogApp').controller('DashboardCtrl', function($scope) {
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate', 'AngularJS', 'Karma'
-    ];
-});
