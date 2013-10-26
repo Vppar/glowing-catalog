b2c0716fca95f903976e121cdd346699a361ee4e
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-10-26 10:55:31 -0200

    - Done with the partial delivery process.

--- app/scripts/controllers/delivery-details-dialog.js
+++ app/scripts/controllers/delivery-details-dialog.js
@@ -2,10 +2,13 @@
     'use strict';
 
     angular.module('glowingCatalogApp').controller('DeliveryDetailsDialogCtrl', function($scope, dialog, DataProvider) {
+
         // #############################################################################################################
         // Local variables and functions
         // #############################################################################################################
-        var delivery = {};
+        var delivery = {
+            qty : 0
+        };
         var order = dialog.order;
         var selectedItemIdx = dialog.selectedItemIdx;
 
@@ -15,13 +18,27 @@
 
         $scope.delivery = delivery;
         $scope.order = order;
-        $scope.selectedItemIdx = selectedItemIdx;
+        $scope.item = order.items[selectedItemIdx];
         $scope.dataProvider = DataProvider;
         $scope.orderItemDeliveriesFilter = function orderItemDeliveriesFilter(delivery) {
             return delivery.orderId === $scope.order.id;
         };
-        $scope.addToDeliveries = function(index) {
-            delivery.id = DataProvider.deliveries.length + 1;
+        $scope.confirm = function(index) {
+            if (delivery.qty <= 0) {
+                // FIXME - Place a decent dialog here instead of this alert.
+                alert('Não é possível fazer uma entrega sem itens.');
+                return;
+            }
+            var item = order.items[selectedItemIdx];
+            // Be careful this condition depends entirely on the premisse that
+            // this screen will only open after the partial delivery screen.
+            // There the order item is augmented with the delivered attribute.
+            if ((delivery.qty + item.delivered) > item.qty) {
+                // FIXME - Place a decent dialog here instead of this alert.
+                alert('Não é possível fazer uma entrega com mais produtos do que solicitado na ordem.');
+                return;
+            }
+            
             delivery.datetime = new Date();
             delivery.orderId = order.id;
             delivery.item = angular.copy(order.items[selectedItemIdx]);
@@ -29,15 +46,12 @@
             DataProvider.deliveries.push(angular.copy(delivery));
 
             delivery.qty = 0;
+
+            dialog.close();
         };
 
         $scope.closeDialog = function() {
             dialog.close();
         };
-
-        function main() {
-            delivery.qty = 0;
-        }
-        main();
     });
 }(angular));
