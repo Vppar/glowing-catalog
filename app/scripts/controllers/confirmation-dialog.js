cb9b26e5ca2e13dc63694f49c42fc3e498802453
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-10-28 21:26:13 -0200

    Confirmation dialog improvements

--- app/scripts/controllers/confirmation-dialog.js
+++ app/scripts/controllers/confirmation-dialog.js
@@ -1,12 +1,16 @@
 (function(angular) {
     'use strict';
-    angular.module('glowingCatalogApp').controller('ConfirmationDialogCtrl', function($scope, dialog) {
-        
+    angular.module('glowingCatalogApp').controller('ConfirmationDialogCtrl', function($scope, $q, dialog) {
+
         $scope.title = dialog.data.title;
         $scope.message = dialog.data.message;
 
-        $scope.closeDialog = function(result) {
-            dialog.close(result);
+        $scope.confirm = function() {
+            dialog.close();
         };
+        $scope.cancel = function() {
+            dialog.close($q.reject());
+        };
+
     });
 })(angular);
--- app/scripts/controllers/payment.js
+++ app/scripts/controllers/payment.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('glowingCatalogApp').controller(
-            'PaymentCtrl', function($filter, $scope, $dialog, $location, DataProvider, DialogService, OrderService) {
+            'PaymentCtrl', function($filter, $scope, $dialog, $location, $q, DataProvider, DialogService, OrderService) {
 
                 $scope.dataProvider = DataProvider;
                 $scope.customer = DataProvider.customer;
@@ -10,18 +10,20 @@
                 $scope.cash = 0;
                 $scope.productsTotal = 0;
 
-                $scope.confirm = function() {
+                function confirmDialogFactory() {
+                    var openConfirmationDialogIntent = $q.defer();
+                    var openConfirmationDialogPromise = openConfirmationDialogIntent.promise.then(openConfirmationAttempt);
+
+                    $scope.confirm = openConfirmationDialogIntent.resolve;
+                    return openConfirmationDialogPromise;
+                }
+
+                function openConfirmationAttempt() {
                     return DialogService.confirmationDialog({
                         title : 'Confirmar pagamento',
                         message : 'Deseja confirmar o pagamento ?',
-                    }).then(function(result) {
-                        if (result) {
-                            OrderService.placeOrder();
-                            OrderService.createOrder();
-                            $location.path('/');
-                        }
                     });
-                };
+                }
 
                 $scope.filterQty = function(product) {
                     return product.qty;
@@ -57,5 +59,17 @@
                 $scope.goToBasket = function() {
                     $location.path('basket');
                 };
+
+                function main() {
+                    var confirmationDialogPromise = confirmDialogFactory();
+                    confirmationDialogPromise.then(function() {
+                        OrderService.placeOrder();
+                        OrderService.createOrder();
+                        $location.path('/');
+                    }, function() {
+                        main();
+                    });
+                }
+                main();
             });
 }(angular));
