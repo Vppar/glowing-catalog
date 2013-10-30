3c90a99d4f925afae06bd37035974d2872900547
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-10-29 22:43:07 -0200

    Layout fix.

--- app/scripts/controllers/delivery-details-dialog.js
+++ app/scripts/controllers/delivery-details-dialog.js
@@ -1,43 +1,179 @@
 (function(angular) {
     'use strict';
 
-    angular.module('glowingCatalogApp').controller('DeliveryDetailsDialogCtrl', function($scope, dialog, DataProvider) {
-        // #############################################################################################################
-        // Local variables and functions
-        // #############################################################################################################
-        var delivery = {};
-        var order = dialog.order;
-        var selectedItemIdx = dialog.selectedItemIdx;
+    angular.module('glowingCatalogApp').controller(
+            'DeliveryDetailsDialogCtrl',
+            function($scope, $filter, $timeout, dialog, DataProvider) {
 
-        // #############################################################################################################
-        // Scope variables and functions
-        // #############################################################################################################
+                // #############################################################################################################
+                // Local variables and functions
+                // #############################################################################################################
+                var delivery = {
+                    items : []
+                };
+                var order = dialog.data.order;
+                var timewatcher = {};
 
-        $scope.delivery = delivery;
-        $scope.order = order;
-        $scope.selectedItemIdx = selectedItemIdx;
-        $scope.dataProvider = DataProvider;
-        $scope.orderItemDeliveriesFilter = function orderItemDeliveriesFilter(delivery) {
-            return delivery.orderId === $scope.order.id;
-        };
-        $scope.addToDeliveries = function(index) {
-            delivery.id = DataProvider.deliveries.length + 1;
-            delivery.datetime = new Date();
-            delivery.orderId = order.id;
-            delivery.item = angular.copy(order.items[selectedItemIdx]);
+                // #############################################################################################################
+                // Scope variables and functions
+                // #############################################################################################################
 
-            DataProvider.deliveries.push(angular.copy(delivery));
+                $scope.item = {};
+                $scope.order = order;
+                $scope.delivery = delivery;
 
-            delivery.qty = 0;
-        };
+                $scope.selectOrderItem = function selectOrderItem(itemId) {
+                    $scope.item.remaining = $filter('filter')(delivery.orderItems, function(item) {
+                        return item.id === Number(itemId);
+                    })[0].remaining;
+                    $scope.item.qty = $scope.item.remaining;
+                };
 
-        $scope.closeDialog = function() {
-            dialog.close();
-        };
+                $scope.itemsRemainingFilter = function itemsRemainingFilter(item) {
+                    return item.remaining > 0;
+                };
 
-        function main() {
-            delivery.qty = 0;
-        }
-        main();
-    });
+                $scope.addToDelivery = function(item) {
+                    var filteredItems = $filter('filter')(delivery.items, function(product) {
+                        return Boolean(product.id === Number(item.id));
+                    });
+
+                    if (filteredItems.length === 0) {
+                        var filteredProducts = $filter('filter')(DataProvider.products, function(product) {
+                            return Boolean(product.id === Number(item.id));
+                        });
+                        var deliveryItem = filteredProducts[0];
+                        deliveryItem.qty = item.qty;
+                        delivery.items.push(angular.copy(deliveryItem));
+
+                        var filteredOrderItems = $filter('filter')(delivery.orderItems, function(product) {
+                            return Boolean(product.id === Number(item.id));
+                        });
+                        filteredOrderItems[0].remaining = filteredOrderItems[0].remaining - item.qty;
+                        
+                        var filteredOrderProducts = $filter('filter')(delivery.orderItems, $scope.itemsRemainingFilter);
+                        angular.extend(item, filteredOrderProducts[0]);
+                    }
+                };
+
+                $scope.removeFromDelivery = function(index) {
+                    var item = $scope.delivery.items[index];
+                    var filteredOrderItems = $filter('filter')(delivery.orderItems, function(product) {
+                        return Boolean(product.id === Number(item.id));
+                    });
+                    filteredOrderItems[0].qty = filteredOrderItems[0].qty + item.qty;
+                    delivery.items.splice(index, 1);
+                };
+
+                $scope.hourChange =
+                        function hourChange() {
+                            if (delivery.hour && delivery.hour.length == 4) {
+                                delivery.datetime =
+                                        new Date(
+                                                delivery.datetime.getFullYear(), delivery.datetime.getMonth(), delivery.datetime.getDate(),
+                                                delivery.hour.substring(0, 2), delivery.hour.substring(2, 4));
+                            }
+                        };
+
+                $scope.cancel = function() {
+                    $timeout.cancel(timewatcher);
+                    dialog.close();
+                };
+                $scope.save =
+                        function() {
+                            if (delivery.items.length === 0) {
+                                alert('Não é possivel agendar uma entrega sem itens.');
+                                return;
+                            }
+
+                            // Get the timestamp from date picker and merge with
+                            // the informed hour
+                            delivery.datetime = convertToUTC(delivery.datetime);
+                            delivery.hour =
+                                    new Date(
+                                            delivery.datetime.getFullYear(), delivery.datetime.getMonth(), delivery.datetime.getDate(),
+                                            delivery.hour.split(':')[0], delivery.hour.split(':')[1], delivery.hour.split(':')[2]);
+
+                            delivery.orderId = order.id;
+                            delivery.status = 'scheduled';
+                            delivery.items = angular.copy(delivery.items);
+
+                            if (delivery.id) {
+                                var recoveredDelivery = $filter('filter')(DataProvider.deliveries, function(item) {
+                                    return item.id === delivery.id;
+                                })[0];
+                                angular.extend(recoveredDelivery, delivery);
+                            } else {
+                                delivery.id = DataProvider.deliveries.length + 1;
+                                DataProvider.deliveries.push(angular.copy(delivery));
+                            }
+
+                            delete delivery.qty;
+                            delete delivery.items;
+
+                            $timeout.cancel(timewatcher);
+
+                            dialog.close();
+                        };
+
+                $scope.deliver = function() {
+                    if (delivery.items.length === 0) {
+                        alert('Não é possivel registrar uma entrega sem itens.');
+                        return;
+                    }
+
+                    delivery.id = DataProvider.deliveries.length + 1;
+                    delivery.datetime = convertToUTC(new Date());
+                    delivery.orderId = order.id;
+                    delivery.status = 'delivered';
+                    delivery.items = angular.copy(delivery.items);
+
+                    DataProvider.deliveries.push(angular.copy(delivery));
+
+                    delete delivery.qty;
+                    delete delivery.items;
+
+                    $timeout.cancel(timewatcher);
+
+                    dialog.close();
+                };
+
+                function convertToUTC(dt) {
+                    var localDate = new Date(dt);
+                    var localTime = localDate.getTime();
+                    var localOffset = localDate.getTimezoneOffset() * 60000;
+                    return new Date(localTime + localOffset);
+                }
+
+                function timewatch() {
+                    $scope.now = new Date();
+                    timewatcher = $timeout(timewatch, 1000);
+                }
+
+                function main() {
+                    // Set the default qty value based on the first product of
+                    // the list.
+                    for ( var idx in order.items) {
+                        if (order.items[idx].remaining > 0) {
+                            $scope.item.remaining = order.items[idx].remaining;
+                            $scope.item.qty = $scope.item.remaining;
+                            break;
+                        }
+                    }
+                    delivery.orderItems = angular.copy($scope.order.items);
+                    if ($scope.order.selectedDelivery) {
+                        delivery.id = $scope.order.selectedDelivery.id;
+                        delivery.datetime = $scope.order.selectedDelivery.datetime;
+                        delivery.hour = $filter('date')(delivery.datetime, 'HHmm');
+                        delivery.status = $scope.order.selectedDelivery.status;
+                        delivery.items = angular.copy($scope.order.selectedDelivery.items);
+                    } else {
+                        delivery.datetime = new Date();
+                        delivery.hour = $filter('date')(delivery.datetime, 'HHmm');
+                    }
+
+                    timewatch();
+                }
+                main();
+            });
 }(angular));
--- app/scripts/controllers/partial-delivery.js
+++ app/scripts/controllers/partial-delivery.js
@@ -1,46 +1,99 @@
 (function(angular) {
     'use strict';
 
-    angular.module('glowingCatalogApp').controller('PartialDeliveryCtrl', function($scope, $location, $filter, $dialog, DataProvider) {
+    angular.module('glowingCatalogApp').controller(
+            'PartialDeliveryCtrl', function($scope, $location, $filter, $dialog, DataProvider, DialogService) {
 
-        var dataProvider = DataProvider;
+                // #############################################################################################################
+                // Scope variables and functions
+                // #############################################################################################################
+                $scope.order = {};
+                $scope.dataProvider = DataProvider;
+                $scope.partialDeliveryAugmenter = function partialDeliveryAugmenter(orderItem) {
+                    // Keep all deliveries of this order that has its status
+                    // scheduled
+                    var scheduled = $filter('filter')(DataProvider.deliveries, function(delivery) {
+                        return (delivery.orderId === $scope.order.id && delivery.status === 'scheduled');
+                    });
+                    var delivered = $filter('filter')(DataProvider.deliveries, function(delivery) {
+                        return (delivery.orderId === $scope.order.id && delivery.status === 'delivered');
+                    });
 
-        // #############################################################################################################
-        // Scope variables and fuctions
-        // #############################################################################################################
-        $scope.order = {};
-        $scope.dataProvider = dataProvider;
-        $scope.itemDeliveredAugmenter = function itemDeliveredAugmenter(item) {
-            var itemDeliveries = $filter('filter')(dataProvider.deliveries, function(delivery) {
-                return (delivery.orderId === $scope.order.id && delivery.item.id === item.id);
-            });
-            item.delivered = $filter('sum')(itemDeliveries, 'qty');
-            return true;
-        };
-        $scope.openDeliveryDetails = function(index) {
-            var d = $dialog.dialog({
-                backdropClick : true,
-                dialogClass : 'modal'
-            });
-            d.order = $scope.order;
-            d.selectedItemIdx = index;
-            d.open('views/parts/partial-delivery/delivery-details-dialog.html', 'DeliveryDetailsDialogCtrl');
-        };
+                    var items = {};
+                    var scheduledItems = [];
+                    var deliveredItems = [];
 
-        // #############################################################################################################
-        // Local variables and methods.
-        // #############################################################################################################
-        // #############################################################################################################
-        // Main method, controls the flow of this process.
-        // #############################################################################################################
-        function main() {
-            var search = $location.search();
-            var filteredOrders = $filter('filter')(dataProvider.orders, function(item) {
-                return item.id === search.id;
+                    var itemFilter = function itemFilter(item) {
+                        return (item.id === orderItem.id);
+                    };
+
+                    // Get all delivery items that match to this one
+                    for ( var idx in scheduled) {
+                        items = $filter('filter')(scheduled[idx].items, itemFilter);
+                        scheduledItems = scheduledItems.concat(items);
+                    }
+                    for (idx in delivered) {
+                        items = $filter('filter')(delivered[idx].items, itemFilter);
+                        deliveredItems = deliveredItems.concat(items);
+                    }
+
+                    orderItem.scheduled = $filter('sum')(scheduledItems, 'qty');
+                    orderItem.delivered = $filter('sum')(deliveredItems, 'qty');
+                    orderItem.remaining = orderItem.qty - (orderItem.scheduled + orderItem.delivered);
+                    return true;
+                };
+                $scope.orderDeliveriesFilter = function orderDeliveriesFilter(delivery) {
+                    return Boolean(delivery.orderId === $scope.order.id);
+                };
+                $scope.openDeliveryDetails = function(delivery) {
+                    var remainingItems = $filter('filter')($scope.order.items, function(item) {
+                        return item.remaining > 0;
+                    });
+                    if (remainingItems.length === 0 && delivery) {
+                        alert('Não há mais items a serem entregues nesse pedido.');
+                        return;
+                    }
+                    $scope.order.selectedDelivery = delivery;
+                    DialogService.openDialogDeliveryDetails({
+                        order : $scope.order
+                    });
+                };
+
+                /* DIALOG CUSTOMER INFO */
+                $scope.openDialogCustomerInfo = function() {
+                    DialogService.openDialogCustomerInfo({
+                        customer : $scope.order.customer
+                    });
+                };
+
+                $scope.statusNameAugmenter = function statusNameAugmenter(delivery) {
+                    var statusName = '';
+                    if (delivery.status === 'scheduled') {
+                        statusName = 'Agendado';
+                    } else if (delivery.status === 'delivered') {
+                        statusName = 'Entregue';
+                    }
+                    delivery.statusName = statusName;
+                    return true;
+                };
+
+                // #############################################################################################################
+                // Main method, controls the flow of this process.
+                // #############################################################################################################
+                function main() {
+                    var search = $location.search();
+                    var filteredOrders = $filter('filter')(DataProvider.orders, function(item) {
+                        return item.id === Number(search.id);
+                    });
+                    // "There can be only one!" by Connor MacLeod
+                    $scope.order = filteredOrders[0];
+
+                    var filteredCustomers = $filter('filter')(DataProvider.customers, function(item) {
+                        return item.id === $scope.order.customerId;
+                    });
+                    // "There can be only one!" by Connor MacLeod
+                    $scope.order.customer = filteredCustomers[0];
+                }
+                main();
             });
-            // Like Connor MacLeod said once, "There can be only one!"
-            $scope.order = filteredOrders[0];
-        }
-        main();
-    });
 }(angular));
