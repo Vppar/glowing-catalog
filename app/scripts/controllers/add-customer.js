33539da86ec6a3f2891b9fe16f5f1b49fcd00ca8
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-11-09 14:46:26 -0200

    Just stated add customer controller migration.

--- app/scripts/controllers/add-customer.js
+++ app/scripts/controllers/add-customer.js
@@ -1,74 +1,75 @@
 (function(angular) {
     'use strict';
 
-    angular.module('glowingCatalogApp').controller(
-            'AddCustomerCtrl', function($scope, $dialog, $location, DataProvider, DialogService, OrderService) {
+    angular.module('tnt.catalog.customer.add', [
+        'tnt.catalog'
+    ]).controller('AddCustomerCtrl', function($scope, $dialog, $location, DataProvider, DialogService, OrderService) {
 
-                // ############################################################################################################
-                // Scope binding variables
-                // ############################################################################################################
+        // ############################################################################################################
+        // Scope binding variables
+        // ############################################################################################################
 
-                $scope.dataProvider = DataProvider;
+        $scope.dataProvider = DataProvider;
 
-                $scope.failed = false;
+        $scope.failed = false;
 
-                $scope.customer = {
-                    address : {},
-                    birthday : {},
-                    emails : [
-                        {
-                            address : ''
-                        }
-                    ],
-                    phones : [
-                        {
-                            number : ''
-                        }
-                    ]
-                };
-                var customer = $scope.customer;
+        $scope.customer = {
+            address : {},
+            birthday : {},
+            emails : [
+                {
+                    address : ''
+                }
+            ],
+            phones : [
+                {
+                    number : ''
+                }
+            ]
+        };
+        var customer = $scope.customer;
 
-                // ############################################################################################################
-                // Scope functions
-                // ############################################################################################################
+        // ############################################################################################################
+        // Scope functions
+        // ############################################################################################################
 
-                $scope.openDialogAddCustomerTels = function openDialogAddCustomerTels(phones) {
-                    DialogService.openDialogAddCustomerTels({
-                        phones : phones
-                    }).then(function resultPhones(phones) {
-                        customer.phones = phones;
-                    });
-                };
-
-                $scope.openDialogAddCustomerEmails = function openDialogAddCustomerEmails(emails) {
-                    DialogService.openDialogAddCustomerEmails({
-                        emails : emails
-                    }).then(function resultEmails(emails) {
-                        customer.emails = emails;
-                    });
-                };
-
-                $scope.confirm = function confirm() {
-                    $scope.failed = true;
-                    if (!$scope.customerForm.$valid) {
-                        DialogService.messageDialog({
-                            title : 'Novo usuário',
-                            message : 'Os campos destacados são de preenchimento obrigatório.',
-                            btnYes : 'OK'
-                        });
-                        return;
-                    }
-                    customer.id = DataProvider.customers.length + 1;
-                    DataProvider.customers.push(customer);
-                    DataProvider.customers.sort(function(x, y) {
-                        return ((x.name === y.name) ? 0 : ((x.name > y.name) ? 1 : -1));
-                    });
-                    OrderService.order.customerId = customer.id;
-                    $location.path('/');
-                };
-
-                $scope.goToBasket = function() {
-                    $location.path('basket');
-                };
+        $scope.openDialogAddCustomerTels = function openDialogAddCustomerTels(phones) {
+            DialogService.openDialogAddCustomerTels({
+                phones : phones
+            }).then(function resultPhones(phones) {
+                customer.phones = phones;
             });
+        };
+
+        $scope.openDialogAddCustomerEmails = function openDialogAddCustomerEmails(emails) {
+            DialogService.openDialogAddCustomerEmails({
+                emails : emails
+            }).then(function resultEmails(emails) {
+                customer.emails = emails;
+            });
+        };
+
+        $scope.confirm = function confirm() {
+            $scope.failed = true;
+            if (!$scope.customerForm.$valid) {
+                DialogService.messageDialog({
+                    title : 'Novo usuário',
+                    message : 'Os campos destacados são de preenchimento obrigatório.',
+                    btnYes : 'OK'
+                });
+                return;
+            }
+            customer.id = DataProvider.customers.length + 1;
+            DataProvider.customers.push(customer);
+            DataProvider.customers.sort(function(x, y) {
+                return ((x.name === y.name) ? 0 : ((x.name > y.name) ? 1 : -1));
+            });
+            OrderService.order.customerId = customer.id;
+            $location.path('/');
+        };
+
+        $scope.goToBasket = function() {
+            $location.path('basket');
+        };
+    });
 }(angular));
