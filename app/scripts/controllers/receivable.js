9033cb270ba5b9b5fba022f7eaac21c13f4ef64e
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-11-25 14:11:36 -0200

    MK-1: Implement ReceivableCtrl.isValid tests - Tests implemented.

---
+++ app/scripts/controllers/receivable.js
@@ -0,0 +1,10 @@
+
+(function(angular) {
+    'use strict';
+
+    angular.module('tnt.catalog.financial.receivable', []).controller('ReceivableCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate', 'AngularJS', 'Karma'
+        ];
+    });
+}(angular));
