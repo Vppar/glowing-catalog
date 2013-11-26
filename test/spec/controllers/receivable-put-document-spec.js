34d3f1e47ceb79b5fbc57f934b38fe80474271b5
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-11-26 15:06:13 -0200

    MK-6: Implement ReceivableCtrl.putDocument tests - Done.

---
+++ test/spec/controllers/receivable-put-document-spec.js
@@ -0,0 +1,74 @@
+describe('Controller: ReceivableCtrl', function() {
+
+    // load the controller's module
+    beforeEach(function() {
+        module('tnt.catalog.financial.receivable');
+    });
+
+    var scope = {};
+    var document = {};
+    var log = {};
+    var rs = {};
+    
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope) {
+        // $scope mock
+        scope = $rootScope.$new();
+
+        // $log mock
+        log.error = jasmine.createSpy('$log.error');
+        
+        // ReceivableService mock
+        rs.update = jasmine.createSpy('ReceivableService.update');
+        
+        $controller('ReceivableCtrl', {
+            $scope : scope,
+            $log : log, 
+            ReceivableService : rs
+        });
+    }));
+    
+    /**
+     * Given a valid document
+     * when an attach is triggered
+     * then the document must be attached
+     */
+    it('should attach a document', function() {
+        // given
+        document = jasmine.createSpy('document.isValid').andReturn(true);
+        
+        var receivable = {};
+        angular.extend(receivable, scope);
+        
+        // when
+        scope.attach(document);
+        receivable.received = document;
+        
+        // then
+        expect(rs.update).toHaveBeenCalledWith(receivable);
+    });
+    
+    /**
+     * Given an invalid document
+     * when an attach is triggered
+     * then the user must be warned: invalid document
+     */
+    it('shouldn\'t fulfill a canceled receivable', function() {
+     // given
+        document = jasmine.createSpy('document.isValid').andReturn(true);
+        
+        var receivable = {};
+        angular.extend(receivable, scope);
+        
+        // when
+        scope.attach(document);
+        receivable.received = document;
+        
+        // then
+        expect(rs.update).not.toHaveBeenCalled();
+        expect(log.error).toHaveBeenCalledWith('ReceivableCtrl: -Invalid document '+JSON.stringify(document));
+        expect(DialogService.messageDialog).toHaveBeenCalledWith({title: 'Contas à Pagar', message: 'Documento Inválido', btnYes: 'OK'});
+    });
+    
+});
