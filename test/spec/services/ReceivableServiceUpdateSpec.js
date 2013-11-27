41428730792258b94bdc1e88a235fec457a8fc2c
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-11-27 20:38:00 -0200

    MK-10: Implement ReceivableService.update tests - Done.

--- test/spec/services/ReceivableServiceUpdateSpec.js
+++ test/spec/services/ReceivableServiceUpdateSpec.js
@@ -1,15 +1,19 @@
 describe('Service: PaymentServiceSpec', function() {
 
-    var receivable = {id: 1};
+    var receivableCtrl = {};
+    var log = {};
 
     // load the service's module
     beforeEach(function() {
+        receivableCtrl = {mock: 'I\'m mock 2', receivable:{id: 2}};
         var mock = {
-            receivables : [angular.copy(receivable)]
+            receivables : [{mock: 'I\'m mock', receivable:{id: 1}}]
         };
+        log.error = jasmine.createSpy('$log.error');
         module('tnt.catalog.service.receivable');
         module(function($provide) {
             $provide.value('DataProvider', mock);
+            $provide.value('$log', log);
         });
     });
     beforeEach(inject(function(_DataProvider_, _ReceivableService_) {
@@ -25,8 +29,19 @@ describe('Service: PaymentServiceSpec', function() {
      * then a receivable must be update in the database
      */
     it('should update a receivable', function() {
-        receivable.isValid = jasmine.createSpy('ReceivableCtrl.isValid').andReturn(true);
+        // given
+        receivableCtrl.isValid = jasmine.createSpy('ReceivableCtrl.isValid').andReturn(true);
+        DataProvider.receivables.push(angular.copy(receivableCtrl));
         
+        // when
+        receivableCtrl.mock = 'I\'m a new mock 2';
+        ReceivableService.update(receivableCtrl);
+        
+        var lastReceivable = DataProvider.receivables.pop();
+        
+        // then
+        expect(receivableCtrl.isValid).toHaveBeenCalled();
+        expect(lastReceivable).toEqual(receivableCtrl);
     });
     
     /**
@@ -36,6 +51,19 @@ describe('Service: PaymentServiceSpec', function() {
      * then we must log: Could not find receivable to update
      */
     it('shouldn\'t update a receivable that is not present', function() {
+        // given
+        receivableCtrl.isValid = jasmine.createSpy('ReceivableCtrl.isValid').andReturn(true);
+        
+        // when
+        receivableCtrl.mock = 'I\'m a new mock 2';
+        ReceivableService.update(receivableCtrl);
+        
+        var lastReceivable = DataProvider.receivables.pop();
+
+        // then
+        expect(receivableCtrl.isValid).toHaveBeenCalled();
+        expect(lastReceivable).not.toEqual(receivableCtrl);
+        expect(log.error).toHaveBeenCalledWith('Could not find receivable to update');
     });
     
     /**
@@ -44,5 +72,18 @@ describe('Service: PaymentServiceSpec', function() {
      * then we must log: invalid receivable: {}
      */
     it('shouldn\'t update an invalid receivable', function() {
+     // given
+        receivableCtrl.isValid = jasmine.createSpy('ReceivableCtrl.isValid').andReturn(false);
+        
+        // when
+        receivableCtrl.mock = 'I\'m a new mock 2';
+        ReceivableService.update(receivableCtrl);
+        
+        var lastReceivable = DataProvider.receivables.pop();
+
+        // then
+        expect(receivableCtrl.isValid).toHaveBeenCalled();
+        expect(lastReceivable).not.toEqual(receivableCtrl);
+        expect(log.error).toHaveBeenCalledWith('ReceivableService: -Invalid receivable:' + JSON.stringify(receivableCtrl));
     });
 });
