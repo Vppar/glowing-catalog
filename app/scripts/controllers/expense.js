d04027554b80bfe790132d14d58cb589b90297ca
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-11-28 17:54:07 -0200

    - Expense and receivable routes.

---
+++ app/scripts/controllers/expense.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('glowingCatalogApp')
+  .controller('ExpenseCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
