266f2334bc38ea2a884c2fa34a0a18144e0d1e97
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-11-28 18:56:27 -0200

    - Renaming services functions names.

--- app/scripts/services/ExpenseService.js
+++ app/scripts/services/ExpenseService.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('glowingCatalogApp')
-  .service('ExpenseService', function Expenseservice() {
+  .service('ExpenseService', function ExpenseService() {
     // AngularJS will instantiate a singleton by calling "new" on this function
   });
--- app/scripts/services/ReceivableService.js
+++ app/scripts/services/ReceivableService.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('glowingCatalogApp')
-  .service('ReceivableService', function Expenseservice() {
+  .service('ReceivableService', function ExpenseService() {
     // AngularJS will instantiate a singleton by calling "new" on this function
   });
