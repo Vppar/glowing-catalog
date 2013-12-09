ea73cada8b8a0d388e3a90ca23a5e00e2019f321
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-12-09 10:38:14 -0200

    ReceivableCtrl  - cancel.

--- app/scripts/controllers/receivable.js
+++ app/scripts/controllers/receivable.js
@@ -1,10 +1,62 @@
-
 (function(angular) {
     'use strict';
 
-    angular.module('tnt.catalog.financial.receivable', []).controller('ReceivableCtrl', function($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate', 'AngularJS', 'Karma'
-        ];
+    /**
+     * Controller that encapsulates a receivable and handle its operations.
+     * 
+     * @author Arnaldo S. Rodrigues Jr.
+     */
+    angular.module('tnt.catalog.financial.receivable', []).controller('ReceivableCtrl', function($scope, $log, ReceivableService) {
+
+        // #############################################################################################################
+        // Local variables
+        // #############################################################################################################
+        /**
+         * Target receivable to all controller operations.
+         */
+        var receivable = {};
+        /**
+         * Easy the access to the ReceivableService inside the controller.
+         */
+        var service = ReceivableService;
+
+        // #############################################################################################################
+        // Receivable functions
+        // #############################################################################################################
+
+        /**
+         * Cancel a receivable.
+         */
+        var cancel = function cancel() {
+            var result = false;
+            if (receivable.id) {
+                if (!receivable.received) {
+                    receivable.canceled = true;
+                    result = service.update(receivable);
+                } else {
+                    $log.error('ReceivableCtrl: -Unable to cancel an already fulfilled receivable.');
+                }
+            } else {
+                $log.error('ReceivableCtrl: -Unable to cancel a receivable that was not saved.');
+            }
+            return result;
+        };
+
+        /**
+         * Verifies if a receivable is valid.
+         */
+        var isValid = function isValid() {
+            return true;
+        };
+
+        // #############################################################################################################
+        // Scope variables
+        // #############################################################################################################
+        /**
+         * Exposes the methods in the scope.
+         */
+        $scope.receivable = receivable;
+        $scope.cancel = cancel;
     });
+
 }(angular));
