5fba023b3556bfdb1e3ae57f7509a81787e35649
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-12-15 18:55:35 -0200

    Receivable screen reorganized.

--- app/scripts/controllers/receivable.js
+++ app/scripts/controllers/receivable.js
@@ -3,9 +3,23 @@
     angular.module('tnt.catalog.financial.expense', []).controller('ReceivableCtrl', function($scope, ReceivableService) {
 
         /**
-         * Expenses list
+         * Receivables list.
          */
         $scope.receivables = ReceivableService.receivables();
+
+        /**
+         * Entities list to augment expenses.
+         */
         $scope.entities = ReceivableService.entities();
+
+        /**
+         * Controls which fragment will be shown.
+         */
+        $scope.selectedReceivableMode = 'read';
+        
+        
+        $scope.selectReceivableMode = function selectReceivableMode(selectedMode){
+            $scope.selectedReceivableMode = selectedMode;
+        };
     });
 }(angular));
