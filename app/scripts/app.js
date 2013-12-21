44057e42e9523c25ca23a135e89aeb52527d4d5f
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2013-12-20 23:27:37 -0200

    Num pad now is a directive.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,11 +4,11 @@
     angular.module(
             'glowingCatalogApp',
             [
-                'ui.bootstrap', 'ui.select2', 'tnt.catalog.filter.count', 'tnt.catalog.filter.findBy', 'tnt.catalog.filter.sum',
-                'tnt.catalog.filter.paymentType', 'tnt.catalog.service.data', 'tnt.catalog.service.dialog', 'tnt.catalog.service.order',
-                'tnt.catalog.service.expense', 'tnt.catalog.service.payment', 'tnt.catalog.service.sms', 'tnt.catalog.service.receivable',
-                'tnt.catalog.service.storage', 'tnt.catalog.header', 'tnt.catalog.basket', 'tnt.catalog.basket.add',
-                'tnt.catalog.customer', 'tnt.catalog.customer.add.phones', 'tnt.catalog.customer.add.emails',
+                'ui.bootstrap', 'ui.select2', 'tnt.catalog.directive.numpad', 'tnt.catalog.filter.count', 'tnt.catalog.filter.findBy',
+                'tnt.catalog.filter.sum', 'tnt.catalog.filter.paymentType', 'tnt.catalog.service.data', 'tnt.catalog.service.dialog',
+                'tnt.catalog.service.order', 'tnt.catalog.service.expense', 'tnt.catalog.service.payment', 'tnt.catalog.service.sms',
+                'tnt.catalog.service.receivable', 'tnt.catalog.service.storage', 'tnt.catalog.header', 'tnt.catalog.basket',
+                'tnt.catalog.basket.add', 'tnt.catalog.customer', 'tnt.catalog.customer.add.phones', 'tnt.catalog.customer.add.emails',
                 'tnt.catalog.customer.choose', 'tnt.catalog.payment', 'tnt.catalog.payment.check', 'tnt.catalog.payment.creditcard',
                 'tnt.catalog.payment.exchange', 'tnt.catalog.product.input.dialog', 'tnt.catalog.financial.receivable',
                 'tnt.catalog.financial.receivable.entity', 'tnt.catalog.financial.expense', 'tnt.catalog.financial.incomeStatement'
