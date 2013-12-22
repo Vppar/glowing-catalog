8b6eaee072f4630f5fc77d1f6577e704d2490efa
wesleyakio <wesleyakio@tuntscorp.com>
2013-12-22 17:33:49 -0200

    Touch browser click delay fix

--- app/scripts/directives/numPad.js
+++ app/scripts/directives/numPad.js
@@ -3,13 +3,13 @@
 
     var templateUrl = 'views/parts/catalog/num-pad.html';
 
-    angular.module('tnt.catalog.directive.numpad',[]).run(function($http, $templateCache) {
+    angular.module('tnt.catalog.directive.numpad', []).run(function($http, $templateCache) {
         $http.get(templateUrl, {
             cache : $templateCache
         });
     });
 
-    angular.module('tnt.catalog.directive.numpad',[]).directive('numPad', function() {
+    angular.module('tnt.catalog.directive.numpad', []).directive('numPad', function() {
         return {
             restrict : 'E',
             scope : {
@@ -18,7 +18,50 @@
             },
             templateUrl : templateUrl,
             link : function postLink(scope, element, attrs) {
-                console.log(scope.done);
+
+                element.find("img[alt='0']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(0)');
+                });
+                element.find("img[alt='00']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(00)');
+                });
+                element.find("img[alt='1']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(1)');
+                });
+                element.find("img[alt='2']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(2)');
+                });
+                element.find("img[alt='3']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(3)');
+                });
+                element.find("img[alt='4']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(4)');
+                });
+                element.find("img[alt='5']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(5)');
+                });
+                element.find("img[alt='6']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(6)');
+                });
+                element.find("img[alt='7']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(7)');
+                });
+                element.find("img[alt='8']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(8)');
+                });
+                element.find("img[alt='9']").bind('tap', function() {
+                    scope.$apply('pushMoneyDigit(9)');
+                });
+                element.find("img[alt='backspace']").bind('tap', function() {
+                    scope.$apply('removeMoneyDigit()');
+                });
+                element.find("img[alt='clear']").bind('tap', function() {
+                    scope.$apply('clearMoney()');
+                });
+                element.find("img[alt='ok']").bind('tap', function() {
+                    scope.$apply('btnOk()');
+                });
+
                 scope.pushMoneyDigit = function pushMoneyDigit(digit) {
                     scope.amount += digit;
                     scope.amount = shiftPoint(scope.amount);
