db870f14f38cdc13ef9a666a832800f38e2641d1
thiagoclassen <russo.ec@gmail.com>
2014-01-06 17:40:27 -0200

    Created the StockKeeper tests.

---
+++ test/spec/services/StockKeeper/StockKeeperRemoveSpec.js
@@ -0,0 +1,77 @@
+'use strict';
+
+describe('Service: StockKeeper', function() {
+
+    var jKeeper = {};
+    
+    // load the service's module
+    beforeEach(function() {
+        module('tnt.catalog.stock');
+        module('tnt.catalog.journal.replayer');
+        module('tnt.catalog.journal');
+    });
+
+    beforeEach(function() {
+        jKeeper.compose = jasmine.createSpy('JournalKeeper.compose');
+        
+        module(function($provide) {
+            $provide.value('JournalKeeper', jKeeper);
+        });
+    });
+
+    // instantiate service
+    var StockKeeper = undefined;
+    beforeEach(inject(function(_StockKeeper_) {
+        StockKeeper = _StockKeeper_;
+    }));
+    
+    /**
+     * <pre>
+     * @spec StockKeeper.remove#1
+     * Given a valid productId
+     * and a positive quantity
+     * when and add is triggered
+     * then a journal entry must be created
+     * an the entry must be registered
+     * </pre>
+     */
+    it('should remove', function() {
+
+        var pId = 23;
+        var qty = 1;
+        var ct = 0;
+//        var ev = {productId: pId, quantity: qty, cost: ct};
+//        var stp = (new Date()).getTime() / 1000;
+//        var entry = {
+//                sequence: null,
+//                stamp: stp,
+//                type: 'stockRemove', 
+//                version: 1,
+//                event: ev
+//                };
+
+        expect(function() {
+            StockKeeper.add(pId, qty, ct);}).not.toThrow();
+//        expect(jKeeper.compose).toHaveBeenCalledWith(entry);
+        expect(jKeeper.compose).toHaveBeenCalled();
+    });
+    
+    /**
+     * <pre>
+     * @spec StockKeeper.remove#2
+     * Given a negative quantity
+     * when and add is triggered
+     * then an error must be raised
+     * </pre>
+     */
+    it('should throw error', function() {
+
+        var pId = 23;
+        var qty = -1;
+        var ct = 0;
+
+        expect(function() {
+            StockKeeper.add(pId, qty, ct);}).toThrow();
+    });
+
+});
