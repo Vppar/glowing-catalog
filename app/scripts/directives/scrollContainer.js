621fc07586c0ca7b47cc66f1aa1c10e3dddb95de
wesleyakio <wesleyakio@tuntscorp.com>
2014-01-06 01:18:44 -0200

    Taking a few things out of the linker and to the control

--- app/scripts/directives/scrollContainer.js
+++ app/scripts/directives/scrollContainer.js
@@ -1,11 +1,13 @@
-'use strict';
+(function(angular) {
+    'use strict';
 
-angular.module('glowingCatalogApp').directive('scrollContainer', function() {
-    return {
-        restrict : 'A',
-        require : '^scrollSpy',
-        link : function postLink(scope, element, attrs, ctrl) {
-            ctrl.registerContainer(attrs.id);
-        }
-    };
-});
+    angular.module('glowingCatalogApp').directive('scrollContainer', function() {
+        return {
+            restrict : 'A',
+            require : '^scrollSpy',
+            link : function postLink(scope, element, attrs, ctrl) {
+                ctrl.registerContainer(attrs.id);
+            }
+        };
+    });
+})(angular);
