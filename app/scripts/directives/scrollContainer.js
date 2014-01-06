7e6052d49b791132330618143a3fb87e7cfb0d54
wesleyakio <wesleyakio@tuntscorp.com>
2014-01-05 23:12:58 -0200

    Created scrollContainer Directive to solve bugs introduced by ng-switch

---
+++ app/scripts/directives/scrollContainer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('glowingCatalogApp').directive('scrollContainer', function() {
+    return {
+        restrict : 'A',
+        require : '^scrollSpy',
+        link : function postLink(scope, element, attrs, ctrl) {
+            ctrl.registerContainer(attrs.id);
+        }
+    };
+});
