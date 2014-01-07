7c0c0a56b70bf2a80964d5a4355be32f25932d56
gustavokira <gustavo.kira@tuntscorp.com>
2014-01-07 13:22:26 -0200

    - WebSQLDriver remove spec finished - WebSQLDriver console.log removed

--- test/spec/services/WebSQLDriver/WebSQLDriverRemoveSpec.js
+++ test/spec/services/WebSQLDriver/WebSQLDriverRemoveSpec.js
@@ -16,8 +16,12 @@ describe('Service: WebSQLDriver.remove', function() {
 		a : 'a3',
 		b : 'b2'
 	};
+	var data4 = {
+			a : 'a2',
+			b : 'b4'
+		};
 	
-	var dataResult = [];
+	
 	
 	var bucketName = 'MyBucket';
 	
@@ -35,8 +39,8 @@ describe('Service: WebSQLDriver.remove', function() {
     beforeEach(function() {
 
         var log = {};
-        log.debug = console.log;
-        log.error = console.log;
+        log.debug = function(){};// console.log;
+        log.error = function(){};//console.log;
 
         module('tnt.storage.websql');
         module(function($provide) {
@@ -59,57 +63,42 @@ describe('Service: WebSQLDriver.remove', function() {
 
     
     it('should remove an object', function() {
-    	createBucket();
-    	persist([data1,data2]);
-    	console.log('dae');
-    });
-    
-    it('should remove an object', function() {
-        
-    });
-    
-    it('should remove an object', function() {
-        
-    });
-    
-    it('should not remove an object', function() {
+    	var resolved = false;
+    	var persisted = false;
+    	var listed = false;
+    	var deleted = false;
     	
-    });
-    
-    var createBucket = function(){
-    	 var resolved = false;
+        var returnedList = null;
+        var dataResult = [];
+        
+        dataResult.push(data2);
+        
+        runs(function() {
 
-         runs(function() {
+            var txBody = function(tx) {
+            	WebSQLDriver.dropBucket(tx,bucketName);
+                WebSQLDriver.createBucket(tx, bucketName, dataCreate, metadata);
+            };
 
-             var txBody = function(tx) {
-                 WebSQLDriver.createBucket(tx, bucketName, dataCreate, metadata);
-             };
+            var promise = WebSQLDriver.transaction(txBody);
 
-             var promise = WebSQLDriver.transaction(txBody);
+            promise.then(function() {
+                resolved = true;
+            });
+        });
 
-             promise.then(function() {
-                 resolved = true;
-             });
-         });
-
-         waitsFor(function() {
-             scope.$apply();
-             return resolved;
-         });
-         
-         return resolved;
-
-    };
-    
-    var persist = function(datas){
-        var persisted = false;
-
+        waitsFor(function() {
+            scope.$apply();
+            return resolved;
+        });
+        
+        
         // persist
         runs(function() {
             var txBody = function(tx) {
-            	for(var d in datas){
-            	WebSQLDriver.persist(tx, bucketName, datas[d]);
-            	}
+            	
+            	WebSQLDriver.persist(tx, bucketName, data1);
+            	WebSQLDriver.persist(tx, bucketName, data2);
             	
             };
 
@@ -125,12 +114,468 @@ describe('Service: WebSQLDriver.remove', function() {
             scope.$apply();
             return persisted;
         });
-
+    	        
         runs(function() {
-            expect(persisted).toEqual(true);
+        	// tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.remove(tx, bucketName,{a:'a1'});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+            
+         // sucess of create
+            promise.then(function() {
+            	deleted = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return deleted;
+        });
+        
+        // list
+        runs(function() {
+        	
+            // tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.list(tx, bucketName).then(function(result){
+            		returnedList = result;	
+            		
+            	});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+
+            // sucess of create
+            promise.then(function() {
+            	listed = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return listed;
+        });
+        
+        runs(function() {
+        	expect(dataResult).toEqual(returnedList);
+        });
+    	
+    });
+    
+    //remove b2
+    it('should remove an object', function() {
+    	var resolved = false;
+    	var persisted = false;
+    	var listed = false;
+    	var deleted = false;
+    	
+        var returnedList = null;
+        var dataResult = [];
+        
+        dataResult.push(data1);
+        
+        runs(function() {
+
+            var txBody = function(tx) {
+            	WebSQLDriver.dropBucket(tx,bucketName);
+                WebSQLDriver.createBucket(tx, bucketName, dataCreate, metadata);
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                resolved = true;
+            });
         });
 
-    };
+        waitsFor(function() {
+            scope.$apply();
+            return resolved;
+        });
+        
+        
+        // persist
+        runs(function() {
+            var txBody = function(tx) {
+            	
+            	WebSQLDriver.persist(tx, bucketName, data1);
+            	WebSQLDriver.persist(tx, bucketName, data2);
+            	WebSQLDriver.persist(tx, bucketName, data3);
+            	
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                persisted = true;
+            });
+
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return persisted;
+        });
+    	        
+        runs(function() {
+        	// tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.remove(tx, bucketName,{b:'b2'});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+            
+         // sucess of create
+            promise.then(function() {
+            	deleted = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return deleted;
+        });
+        
+        // list
+        runs(function() {
+        	
+            // tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.list(tx, bucketName).then(function(result){
+            		returnedList = result;	
+            		
+            	});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+
+            // sucess of create
+            promise.then(function() {
+            	listed = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return listed;
+        });
+        
+        runs(function() {
+        	expect(dataResult).toEqual(returnedList);
+        });
+    });
+    
+    //remove 
+    it('should remove an object', function() {
+       	var resolved = false;
+    	var persisted = false;
+    	var listed = false;
+    	var deleted = false;
+    	
+        var returnedList = null;
+        var dataResult = [];
+        
+        dataResult.push(data1,data3,data4);
+        
+        runs(function() {
+
+            var txBody = function(tx) {
+            	WebSQLDriver.dropBucket(tx,bucketName);
+                WebSQLDriver.createBucket(tx, bucketName, dataCreate, metadata);
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                resolved = true;
+            });
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return resolved;
+        });
+        
+        
+        // persist
+        runs(function() {
+            var txBody = function(tx) {
+            	
+            	WebSQLDriver.persist(tx, bucketName, data1);
+            	WebSQLDriver.persist(tx, bucketName, data2);
+            	WebSQLDriver.persist(tx, bucketName, data3);
+            	WebSQLDriver.persist(tx, bucketName, data4);
+            	
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                persisted = true;
+            });
+
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return persisted;
+        });
+    	        
+        runs(function() {
+        	// tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.remove(tx, bucketName,{a:'a2',b:'b2'});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+            
+         // sucess of create
+            promise.then(function() {
+            	deleted = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return deleted;
+        });
+        
+        // list
+        runs(function() {
+        	
+            // tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.list(tx, bucketName).then(function(result){
+            		returnedList = result;	
+            		
+            	});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+
+            // sucess of create
+            promise.then(function() {
+            	listed = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return listed;
+        });
+        
+        runs(function() {
+        	expect(dataResult).toEqual(returnedList);
+        });
+    });
+    
+    it('should not remove an object', function() {
+    	var resolved = false;
+    	var persisted = false;
+    	var listed = false;
+    	var deleted = false;
+    	
+        var returnedList = null;
+        var dataResult = [];
+        
+        dataResult.push(data1,data2,data3,data4);
+        
+        runs(function() {
+
+            var txBody = function(tx) {
+            	WebSQLDriver.dropBucket(tx,bucketName);
+                WebSQLDriver.createBucket(tx, bucketName, dataCreate, metadata);
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                resolved = true;
+            });
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return resolved;
+        });
+        
+        
+        // persist
+        runs(function() {
+            var txBody = function(tx) {
+            	
+            	WebSQLDriver.persist(tx, bucketName, data1);
+            	WebSQLDriver.persist(tx, bucketName, data2);
+            	WebSQLDriver.persist(tx, bucketName, data3);
+            	WebSQLDriver.persist(tx, bucketName, data4);
+            	
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                persisted = true;
+            });
+
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return persisted;
+        });
+    	        
+        runs(function() {
+        	// tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.remove(tx, bucketName,{a:'a7'});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+            
+         // sucess of create
+            promise.then(function() {
+            	deleted = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return deleted;
+        });
+        
+        // list
+        runs(function() {
+        	
+            // tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.list(tx, bucketName).then(function(result){
+            		returnedList = result;	
+            		
+            	});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+
+            // sucess of create
+            promise.then(function() {
+            	listed = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return listed;
+        });
+        
+        runs(function() {
+        	expect(dataResult).toEqual(returnedList);
+        });
+    });
+    
+    it('should not remove an object', function() {
+    	var resolved = false;
+    	var persisted = false;
+    	var listed = false;
+    	var deleted = false;
+    	
+        var returnedList = null;
+        var dataResult = [];
+        
+        dataResult.push(data1,data2,data3,data4);
+        
+        runs(function() {
+
+            var txBody = function(tx) {
+            	WebSQLDriver.dropBucket(tx,bucketName);
+                WebSQLDriver.createBucket(tx, bucketName, dataCreate, metadata);
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                resolved = true;
+            });
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return resolved;
+        });
+        
+        
+        // persist
+        runs(function() {
+            var txBody = function(tx) {
+            	
+            	WebSQLDriver.persist(tx, bucketName, data1);
+            	WebSQLDriver.persist(tx, bucketName, data2);
+            	WebSQLDriver.persist(tx, bucketName, data3);
+            	WebSQLDriver.persist(tx, bucketName, data4);
+            	
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                persisted = true;
+            });
+
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return persisted;
+        });
+    	        
+        runs(function() {
+        	// tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.remove(tx, bucketName,{a:'a1', b: 'b4'});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+            
+         // sucess of create
+            promise.then(function() {
+            	deleted = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return deleted;
+        });
+        
+        // list
+        runs(function() {
+        	
+            // tx to create a bucket
+            var txBody = function(tx) {
+            	WebSQLDriver.list(tx, bucketName).then(function(result){
+            		returnedList = result;	
+            		
+            	});
+            };
+            // promise from the create
+            var promise = WebSQLDriver.transaction(txBody);
+
+            // sucess of create
+            promise.then(function() {
+            	listed = true;
+            });
+        });
+        
+        waitsFor(function() {
+            scope.$apply();
+            return listed;
+        });
+        
+        runs(function() {
+        	expect(dataResult).toEqual(returnedList);
+        });
+    });
     
     
 });
