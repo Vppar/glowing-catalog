8c720d79f889d8d7373b87e8ebdce42fc38575a9
gustavokira <gustavo.kira@tuntscorp.com>
2014-01-07 11:42:46 -0200

    websqldriver remove spec partial

--- test/spec/services/WebSQLDriver/WebSQLDriverRemoveSpec.js
+++ test/spec/services/WebSQLDriver/WebSQLDriverRemoveSpec.js
@@ -2,6 +2,35 @@
 
 describe('Service: WebSQLDriver.remove', function() {
 
+	var data1 = {
+		a : 'a1',
+		b : 'b1'
+	};
+	
+	var data2 = {
+		a : 'a2',
+		b : 'b2'
+	};
+	
+	var data3 = {
+		a : 'a3',
+		b : 'b2'
+	};
+	
+	var dataResult = [];
+	
+	var bucketName = 'MyBucket';
+	
+	var dataCreate = {
+            a : 'a',
+            b : 'b'
+        };
+	
+	var metadata = {
+            metaVersion : 1
+        };
+	
+	
     // load the service's module
     beforeEach(function() {
 
@@ -23,9 +52,85 @@ describe('Service: WebSQLDriver.remove', function() {
         WebSQLDriver = _WebSQLDriver_;
         scope = $rootScope;
     }));
-
-    it('should be there', function() {
+    
+    it('should be here', function() {
         expect(!!WebSQLDriver).toBe(true);
     });
+
+    
+    it('should remove an object', function() {
+    	createBucket();
+    	persist([data1,data2]);
+    	console.log('dae');
+    });
+    
+    it('should remove an object', function() {
+        
+    });
+    
+    it('should remove an object', function() {
+        
+    });
+    
+    it('should not remove an object', function() {
+    	
+    });
+    
+    var createBucket = function(){
+    	 var resolved = false;
+
+         runs(function() {
+
+             var txBody = function(tx) {
+                 WebSQLDriver.createBucket(tx, bucketName, dataCreate, metadata);
+             };
+
+             var promise = WebSQLDriver.transaction(txBody);
+
+             promise.then(function() {
+                 resolved = true;
+             });
+         });
+
+         waitsFor(function() {
+             scope.$apply();
+             return resolved;
+         });
+         
+         return resolved;
+
+    };
+    
+    var persist = function(datas){
+        var persisted = false;
+
+        // persist
+        runs(function() {
+            var txBody = function(tx) {
+            	for(var d in datas){
+            	WebSQLDriver.persist(tx, bucketName, datas[d]);
+            	}
+            	
+            };
+
+            var promise = WebSQLDriver.transaction(txBody);
+
+            promise.then(function() {
+                persisted = true;
+            });
+
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return persisted;
+        });
+
+        runs(function() {
+            expect(persisted).toEqual(true);
+        });
+
+    };
+    
     
 });
