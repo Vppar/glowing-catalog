bee6a45304b5654f20980e36f634f80576b34d08
thiagoclassen <russo.ec@gmail.com>
2014-01-08 18:08:38 -0200

    Made the specs and the base tests.

--- app/scripts/services/GridKeeper.js
+++ app/scripts/services/GridKeeper.js
@@ -1,6 +1,20 @@
 (function(angular) {
     'use strict';
 
+    /**
+     * 
+     * <pre>
+     * 
+     * @test Grid.constructor#1
+     * Validate the data on the returned object
+     * 
+     * @test Grid.constructor#2
+     * Validate the read only fields
+     * 
+     * </pre>
+     * 
+     */
+    
     angular.module('tnt.catalog.grid.entity', []).factory('Grid', function Grid() {
         var service = function svc(id, grid) {
 
@@ -17,9 +31,27 @@
         return service;
     });
 
-    angular.module('tnt.catalog.grid.keeper', []).service('GridKeeper', function GridKeeper(Grid) {
+    angular.module('tnt.catalog.grid.keeper',['tnt.utils.array']).service('GridKeeper', function GridKeeper(Grid,ArrayUtils) {
         var grid = [];
-
+        /**
+         * <pre>
+         * 
+         *  @spec GridKeeper.build#1
+         *  Given a valid array of products
+         *  when the build is triggered
+         *  then the grid should be populated with the products and their parents 
+         * 
+         *  @spec GridKeeper.build#2
+         *  Given a invalid array of products
+         *  when the build is triggered
+         *  nothing should be added to the grid
+         * 
+         * </pre>
+         * 
+         * Build a grid of items from an array.
+         * 
+         * @param An array of products
+         */
         this.build = function(products) {
             for ( var ix in products) {
                 var product = products[ix];
@@ -37,6 +69,11 @@
                 }
             }
         };
+        
+        this.read = function(){
+            return angular.copy(grid);
+        };
+        
     });
 
     angular.module('tnt.catalog.grid', [
