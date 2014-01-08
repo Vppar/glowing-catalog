8fe779127a3219469b9300ff141ae1a860c6cd79
wesleyakio <wesleyakio@tuntscorp.com>
2014-01-08 11:32:05 -0200

    Catalog improvements:  - fixes for common lineUp breaks;  - Highlights menu

---
+++ app/scripts/services/ProductLineUp.js
@@ -0,0 +1,146 @@
+(function(angular) {
+    'use strict';
+
+    angular.module('glowingCatalogApp').service(
+            'ProductLineUp',
+            function ProductLineUp(ArrayUtils) {
+
+                this.minSize =
+                        function(lineUp) {
+                            for ( var ix in lineUp) {
+                                var product = lineUp[ix];
+
+                                if (angular.isUndefined(product.h)) {
+                                    if ((angular.isUndefined(product.description) || product.description.length < 110) &&
+                                        angular.isUndefined(product.expires) && lineUp.length > 4) {
+                                        product.w = 3;
+                                        product.h = 3;
+                                    } else if (angular.isDefined(product.description) && product.description.length > 175) {
+                                        product.w = 6;
+                                        product.h = 3;
+                                    } else {
+                                        product.w = 6;
+                                        product.h = 2;
+                                    }
+                                }
+                            }
+                        };
+
+                this.lineUp = function(lineUp, left, right) {
+
+                    this.minSize(lineUp);
+
+                    // 3x3 must come in pairs
+                    var _3x3 = ArrayUtils.filter(lineUp, {
+                        w : 3,
+                        h : 3
+                    });
+                    if (_3x3.length % 2) {
+                        var rogue = _3x3.pop();
+                        rogue.w = 6;
+                        rogue.h = 2;
+                    }
+
+                    var leftH = 0;
+                    var rightH = 0;
+
+                    function push(p1, p2) {
+                        if (leftH > rightH) {
+                            right.push(p1);
+                            rightH += p1.h;
+                            if (p2) {
+                                right.push(p2);
+                            }
+                        } else {
+                            left.push(p1);
+                            leftH += p1.h;
+                            if (p2) {
+                                left.push(p2);
+                            }
+                        }
+                    }
+
+                    var cache = false;
+                    for ( var ix in lineUp) {
+                        if (lineUp[ix].w === 3) {
+                            if (cache) {
+                                push(cache, lineUp[ix]);
+                                cache = false;
+                            } else {
+                                cache = lineUp[ix];
+                            }
+                        } else {
+                            push(lineUp[ix]);
+                        }
+                    }
+
+                    if (leftH - rightH === -1) {
+                        fix1(left, right);
+                    }
+
+                    if (rightH - leftH === -1) {
+                        fix1(right, left);
+                    }
+                    
+                    if (leftH - rightH === -2) {
+                        fix2(left, right);
+                    }
+
+                    if (rightH - leftH === -2) {
+                        fix2(right, left);
+                    }
+
+                };
+
+                function fix1(short, big) {
+                    var items = ArrayUtils.list(short, 'h', 2);
+                    if (items.length > 0) {
+                        items[0].h = 3;
+                        return;
+                    }
+
+                    items = ArrayUtils.filter(short, {
+                        w : 3,
+                        h : 3
+                    });
+                    
+                    if (items.length > 1) {
+                        items[0].w = 6;
+                        items[0].h = 2;
+                        items[1].w = 6;
+                        items[1].h = 2;
+                        return;
+                    }
+
+                }
+                
+                function fix2(short, big) {
+                    var items = ArrayUtils.filter(short, {
+                        w : 3,
+                        h : 3
+                    });
+                    
+                    if (items.length > 3) {
+                        items[0].w = 6;
+                        items[0].h = 2;
+                        items[1].w = 6;
+                        items[1].h = 2;
+                        items[2].w = 6;
+                        items[2].h = 2;
+                        items[3].w = 6;
+                        items[3].h = 2;
+                        return;
+                    }
+                    
+                    if (items.length > 1) {
+                        items[0].w = 6;
+                        items[0].h = 2;
+                        items[1].w = 6;
+                        items[1].h = 3;
+                        return;
+                    }
+
+                }
+
+            });
+})(angular);
