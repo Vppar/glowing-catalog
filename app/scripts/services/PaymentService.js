470b5b88f60fe34aa2e91d8fa80dcf735bea5f96
wesleyakio <wesleyakio@tuntscorp.com>
2014-01-14 20:32:06 -0200

    New Payment Entities

--- app/scripts/services/PaymentService.js
+++ app/scripts/services/PaymentService.js
@@ -1,6 +1,38 @@
 (function(angular) {
     'use strict';
 
+    var entities = angular.module('tnt.catalog.payment.entity', []);
+
+    entities.factory('Payment', function Payment() {
+
+        var service = function svc(amount) {
+
+            if (arguments.length != svc.length) {
+                throw 'Payment must be initialized with amount';
+            }
+
+            this.amount = amount;
+        };
+
+        return service;
+    });
+
+    entities.factory('CheckPayment', function CheckPayment(Payment) {
+
+        var service = function svc(amount) {
+
+            if (arguments.length != svc.length) {
+                throw 'CheckPayment must be initialized with amount';
+            }
+
+            ObjectUtils.superInvoke(this, amount);
+        };
+        
+        ObjectUtils.inherits(svc, Payment);
+
+        return service;
+    });
+
     angular.module('tnt.catalog.service.payment', [
         'tnt.catalog.filter.findBy', 'tnt.catalog.service.data'
     ]).service('PaymentService', function PaymentService($filter, DataProvider) {
