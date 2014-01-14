c12a9cb1ec384f970a6f384ab5d4209ba77054a3
gustavokira <gustavo.kira@tuntscorp.com>
2014-01-14 15:58:24 -0200

    VOPP-54 CouponService

---
+++ test/spec/services/Coupon/CouponServiceCancelSpec.js
@@ -0,0 +1,51 @@
+'use strict';
+
+describe('Service: Coupon', function() {
+
+	var voucherStub = {};
+
+	// load the service's module
+	beforeEach(function() {
+		module('tnt.catalog.service.coupon');
+	});
+
+	// define the mocks
+	beforeEach(function() {
+		voucherStub.cancel = jasmine.createSpy('VoucherKeeper.cancel');
+		module(function($provide) {
+			$provide.value('VoucherKeeper', voucherStub);
+		});
+	});
+
+	// instantiate service
+	var CouponService = undefined;
+
+	// inject the dependencies
+	beforeEach(inject(function(_CouponService_) {
+		CouponService = _CouponService_;
+	}));
+
+	it('should cancel the coupon with the passed id', function() {
+		var id = 1;
+		CouponService.cancel(id);
+		expect(voucherStub.cancel).toHaveBeenCalledWith(id, 'coupon');
+	});
+
+	it('should not cancel any coupon with a negative id', function() {
+		var id = -1;
+		expect(function() { CouponService.cancel(id); }).toThrow();
+		expect(voucherStub.cancel).not.toHaveBeenCalled(); 
+	});
+
+	it('should not cancel any coupon with an id equals zero', function() {
+		var id = 0;
+		expect(function() { CouponService.cancel(id); }).toThrow();
+		expect(voucherStub.cancel).not.toHaveBeenCalled();
+	});
+
+	it('should not cancel any coupon with a undefined id', function() {
+		var id = undefined;
+		expect(function() { CouponService.cancel(id); }).toThrow();
+		expect(voucherStub.cancel).not.toHaveBeenCalled();
+	});
+});
