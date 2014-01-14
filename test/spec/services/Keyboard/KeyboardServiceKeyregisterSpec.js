1dd756e4ba2c7755f11c200d04988f6ea07b17ea
gustavokira <gustavo.kira@tuntscorp.com>
2014-01-14 17:33:11 -0200

    KeyboardServiceKeyregisterSpec finished

--- test/spec/services/Keyboard/KeyboardServiceKeyregisterSpec.js
+++ test/spec/services/Keyboard/KeyboardServiceKeyregisterSpec.js
@@ -14,24 +14,63 @@ describe('Service: KeyboardService', function() {
 		expect(!!KeyboardService).toBe(true);
 	});
 
-	xit('should\'t register an input without id', function() {
+	it('should\'t register an input without id', function() {
 
 		var inputmock = {
 
 		};
 
-		KeyboardService.register(inputmock);
+		expect(function() {
+			KeyboardService.register(inputmock);
+		}).toThrow();
 	});
 
-	xit('should register a valid input', function() {
+	it('should register a full valid input', function() {
+
+		var inputmock = {
+			id : 2,
+			prev : 1,
+			next : 3
+		};
+
+		expect(function() {
+			KeyboardService.register(inputmock);
+		}).not.toThrow();
+	});
+
+	it('should register the openKeyboard function in the input', function() {
 		expect(!!KeyboardService).toBe(true);
 	});
 
-	xit('should register the openKeyboard function in the input', function() {
-		expect(!!KeyboardService).toBe(true);
+	it('should register an input with prev', function() {
+		var inputmock = {
+				id : 2,
+				prev : 1,
+			};
+
+			expect(function() {
+				KeyboardService.register(inputmock);
+			}).not.toThrow();
 	});
 
-	xit('should register an input', function() {
+	it('should register an input with next', function() {
+		var inputmock = {
+				id : 2,
+				next : 3,
+			};
 
+			expect(function() {
+				KeyboardService.register(inputmock);
+			}).not.toThrow();
+	});
+
+	it('should register an input withou prev or next', function() {
+		var inputmock = {
+				id : 2
+		};
+
+			expect(function() {
+				KeyboardService.register(inputmock);
+			}).not.toThrow();
 	});
 });
