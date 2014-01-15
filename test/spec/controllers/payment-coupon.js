7cf44beca33b006fdf967f3f56ce1f175190117a
FABIO-DELL <FABIO-DELL@FABIO-DELL-PC>
2014-01-15 17:16:04 -0200

    add some unit tests.

--- test/spec/controllers/payment-coupon.js
+++ test/spec/controllers/payment-coupon.js
@@ -18,14 +18,41 @@ describe('Controller: PaymentCouponCtrl', function() {
         });
     }));
 
-    it('find a proper name', function() {
-        
+    it('Should calculate totals - qty=5 ', function() {
+
         expect(scope.total).toEqual(0);
-        scope.list[0].qty = 5;
-        
+
+        for ( var ix in scope.list) {
+            item = scope.list[ix];
+            item.qty = 5;
+        }
+
         scope.$apply();
-        
-        expect(scope.total).toEqual(25);
-        
+
+        expect(scope.list[0].total).toEqual(25);
+        expect(scope.list[1].total).toEqual(50);
+        expect(scope.list[2].total).toEqual(100);
+        expect(scope.list[3].total).toEqual(150);
+        expect(scope.total).toEqual(325);
+
+    });
+    
+    it('Should calculate totals - qty=23. ', function() {
+
+        expect(scope.total).toEqual(0);
+
+        for ( var ix in scope.list) {
+            item = scope.list[ix];
+            item.qty = 23;
+        }
+
+        scope.$apply();
+
+        expect(scope.list[0].total).toEqual(115);
+        expect(scope.list[1].total).toEqual(230);
+        expect(scope.list[2].total).toEqual(460);
+        expect(scope.list[3].total).toEqual(690);
+        expect(scope.total).toEqual(1495);
+
     });
 });
