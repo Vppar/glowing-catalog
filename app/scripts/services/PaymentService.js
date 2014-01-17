e06683ad773f8d2d748fd8fc4eaa82deca2ff992
arnaldo.rodrigues <arnaldo.rodrigues@sous.com.br>
2014-01-17 16:06:47 -0200

    PaymentService done.

--- app/scripts/services/PaymentService.js
+++ app/scripts/services/PaymentService.js
@@ -3,6 +3,9 @@
 
     var entities = angular.module('tnt.catalog.payment.entity', []);
 
+    /**
+     * Generic payment.
+     */
     entities.factory('Payment', function Payment() {
 
         var service = function svc(amount) {
@@ -17,6 +20,9 @@
         return service;
     });
 
+    /**
+     * Cash payment entity.
+     */
     entities.factory('CashPayment', function CashPayment(Payment) {
 
         var service = function svc(amount) {
@@ -28,6 +34,9 @@
         return service;
     });
 
+    /**
+     * Check payment entity.
+     */
     entities.factory('CheckPayment', function CheckPayment(Payment) {
 
         var service = function svc(amount, bank, agency, account, check, expiration) {
@@ -51,6 +60,9 @@
         return service;
     });
 
+    /**
+     * Credit card payment entity.
+     */
     entities.factory('CreditCardPayment', function CreditCardPayment(Payment) {
 
         var service = function svc(amount, flag, ccNumber, owner, ccDueDate, cvv, cpf, installments) {
@@ -84,6 +96,9 @@
         return service;
     });
 
+    /**
+     * Exchange payment entity.
+     */
     entities.factory('ExchangePayment', function ExchangePayment(Payment) {
 
         var service = function svc(amount) {
@@ -95,6 +110,9 @@
         return service;
     });
 
+    /**
+     * Coupon payment entity.
+     */
     entities.factory('CouponPayment', function CouponPayment(Payment) {
 
         var service = function svc(amount) {
@@ -148,16 +166,40 @@
                     return result;
                 };
 
-                var list = function list() {
+                /**
+                 * Given a type of a payment returns the list of these payments.
+                 * 
+                 * @param typeName - Type of the payments that will be returned.
+                 * @throws Exception - In case of an unknown payment type.
+                 */
+                var list = function list(typeName) {
+                    var paymentList = payments[typeName];
+                    if (!paymentList) {
+                        throw 'Unknow type of payment: typeName=' + typeName;
+                    }
+                    return angular.copy(paymentList);
                 };
 
-                var read = function read() {
+                /**
+                 * Given a type of a payment and an id returns a single payment
+                 * instance.
+                 * 
+                 * @param typeName - Type of the payments that will be returned.
+                 * @throws Exception - In case of an unknown payment type.
+                 */
+                var read = function read(typeName, id) {
+                    // find the list
+                    var paymentList = list(typeName);
+                    // find the payment in the list
+                    var payment = ArrayUtils.find(paymentList, 'id', id);
+
+                    return angular.copy(payment);
                 };
 
                 /**
                  * Adds a payment to temporary list of payments.
                  * 
-                 * @param payment - The new payment to be added.
+                 * @param payment - The payment to be added.
                  * @throws Exception - Throws an exception when the given
                  *             payment isn't of any known type.
                  */
@@ -169,13 +211,44 @@
                     payments[typeName].push(payment);
                 };
 
-                var update = function update() {
+                /**
+                 * Updates the instance of a payment
+                 * 
+                 * @param payment - The payment to be updated.
+                 * @throws Exception - In case of an unknown payment type or
+                 *             unknown payment instance.
+                 */
+                var update = function update(payment) {
+                    // find the list
+                    var paymentList = payments[getTypeName(payment)];
+                    var paymentInstance = ArrayUtils.find(paymentList, 'id', payment.id);
+
+                    angular.extend(paymentInstance, payment);
                 };
 
-                var remove = function remove() {
+                /**
+                 * Given a payment instance remove it.
+                 * 
+                 * @param payment - The payment to be removed.
+                 * @throws Exception - In case of an unknown payment type or
+                 *             unknown payment instance.
+                 */
+                var remove = function remove(payment) {
+                    // find the list
+                    var paymentList = payments[getTypeName(payment)];
+                    // find the payment in the list
+                    var paymentInstance = ArrayUtils.find(paymentList, 'id', payment.id);
+                    // remove
+                    paymentList.splice(paymentList.indexOf(paymentInstance), 1);
                 };
 
+                /**
+                 * Erase all registered payments.
+                 */
                 var clear = function clear() {
+                    for ( var ix in payments) {
+                        payments[ix].length = 0;
+                    }
                 };
 
                 this.add = add;
