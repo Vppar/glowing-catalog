b1a8d65a3ddeb14070c42bd627e6835a03545969
arnaldo.rodrigues <arnaldo.rodrigues@sous.com.br>
2014-01-18 12:11:55 -0200

    Using default orderBy filter.

--- app/scripts/filters/reverse.js
+++
@@ -1,8 +0,0 @@
-(function(angular) {
-    'use strict';
-    angular.module('tnt.catalog.filter.reverse', []).filter('reverse', function() {
-        return function(items) {
-            return items.slice().reverse();
-          };
-        });
-})(angular);
