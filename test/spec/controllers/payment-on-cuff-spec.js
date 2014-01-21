244e5573f0c2ea45421310bd6ef2dc208b2fed12
FABIO-DELL <FABIO-DELL@FABIO-DELL-PC>
2014-01-20 22:24:46 -0200

    Added spec for payment-on-cuff. Added controller to payment-on-cuff. Added controller on index.html

---
+++ test/spec/controllers/payment-on-cuff-spec.js
@@ -0,0 +1,126 @@
+'use strict';
+describe('Controller: PaymentOnCuffCtrl', function() {
+
+    var scope = {};
+    var payments = [];
+    var DialogService = {};
+
+    beforeEach(function() {
+        module('tnt.catalog.filter.findBy');
+        module('tnt.catalog.service.dialog');
+        module('tnt.catalog.service.data');
+        module('tnt.catalog.payment.coupon');
+        module('tnt.catalog.inventory.keeper');
+        module('tnt.catalog.inventory.entity');
+        module('tnt.catalog.service.order');
+
+    });
+
+    beforeEach(inject(function($controller, $rootScope, _$filter_) {
+        // scope mock
+        scope = $rootScope.$new();
+        scope.computeTotals = jasmine.createSpy('scope.computeTotals');
+
+        // reproduce the scope inheritance
+        $controller('PaymentOnCuffCtrl', {
+            $scope : scope,
+            payments : payments,
+            DialogService : DialogService
+        });
+    }));
+
+    /**
+     * Given - 1 as installment
+     * And   - an amount 
+     * And   - dueData 
+     * When  - installments has changed.
+     * Then  - should create many installment as inputed.
+     */
+    it('should create 1 installment', function() {
+        // given
+        scope.amount = 500;
+        scope.dueData = new Date();
+        scope.installmentQty = 1;
+        scope.payments = [];
+        scope.computeInstallments();
+
+        expect(scope.payments.length).toEqual(1);
+        expect(scope.payments[0].amount).toEqual(scope.amount);
+
+    });
+
+    /**
+     * Given - 5 as installment
+     * And   - an amount 
+     * And   - dueData 
+     * When  - installments has changed.
+     * Then  - should create many installment as inputed.
+     */
+    it('should create 5 installment', function() {
+        // given
+        scope.amount = 500;
+        scope.dueDate = new Date();
+        scope.installmentQty = 5;
+
+        var expectedAmount = 100;
+
+        scope.payments = [];
+        scope.computeInstallments();
+        expect(scope.payments.length).toEqual(5);
+        expect(scope.payments[0].amount).toEqual(expectedAmount);
+
+    });
+
+    /**
+     * Given - 6 as installment
+     * And   - an amount 
+     * And   - dueData 
+     * When  - installments has changed.
+     * Then  - should create many installment as inputed.
+     */
+    it('should create 6 installment', function() {
+        // given
+        scope.amount = 500;
+        scope.dueDate = new Date();
+        scope.installmentQty = 6;
+
+        var expectedInstallment1 = 83.33;
+        var expectedInstallment2 = 83.33;
+        var expectedInstallment3 = 83.33;
+        var expectedInstallment4 = 83.33;
+        var expectedInstallment5 = 83.33;
+        var expectedInstallment6 = 83.35;
+
+        var oneMonthAhead = new Date(new Date(scope.dueDate).setMonth(scope.dueDate.getMonth() + 1));
+        var twoeMonthAhead = new Date(new Date(oneMonthAhead).setMonth(oneMonthAhead.getMonth() + 1));
+        var threeMonthAhead = new Date(new Date(twoeMonthAhead).setMonth(twoeMonthAhead.getMonth() + 1));
+        var fourMonthAhead = new Date(new Date(threeMonthAhead).setMonth(threeMonthAhead.getMonth() + 1));
+        var fiveMonthAhead = new Date(new Date(fourMonthAhead).setMonth(fourMonthAhead.getMonth() + 1));
+
+        scope.payments = [];
+
+        scope.computeInstallments();
+
+        expect(scope.payments.length).toEqual(6);
+
+        expect(scope.payments[0].amount).toEqual(expectedInstallment1);
+        expect(scope.payments[0].dueDate).toEqual(scope.dueDate.getTime());
+
+        expect(scope.payments[1].amount).toEqual(expectedInstallment2);
+        expect(scope.payments[1].dueDate).toEqual(oneMonthAhead.getTime());
+
+        expect(scope.payments[2].amount).toEqual(expectedInstallment3);
+        expect(scope.payments[2].dueDate).toEqual(twoeMonthAhead.getTime());
+
+        expect(scope.payments[3].amount).toEqual(expectedInstallment4);
+        expect(scope.payments[3].dueDate).toEqual(threeMonthAhead.getTime());
+
+        expect(scope.payments[4].amount).toEqual(expectedInstallment5);
+        expect(scope.payments[4].dueDate).toEqual(fourMonthAhead.getTime());
+
+        expect(scope.payments[5].amount).toEqual(expectedInstallment6);
+        expect(scope.payments[5].dueDate).toEqual(fiveMonthAhead.getTime());
+
+    });
+
+});
