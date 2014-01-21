07f7a4cbabf0055d77f68a3d60cd97dd095b6d13
muriloquadros <murilo.quadros@tuntscorp.com>
2014-01-21 16:34:49 -0200

    change indentation

--- test/spec/controllers/payment-exchange/payment-exchange-confirm-spec.js
+++ test/spec/controllers/payment-exchange/payment-exchange-confirm-spec.js
@@ -1,6 +1,6 @@
 describe('Controller: PaymentExchangeCtrlConfirm', function() {
 
-    var DialogService ={};
+    var DialogService = {};
     var scope = {};
     var InventoryKeeper = {};
     var ArrayUtils = {};
@@ -12,20 +12,21 @@ describe('Controller: PaymentExchangeCtrlConfirm', function() {
         module('tnt.catalog.inventory.keeper');
         module('tnt.catalog.payment.service');
     });
-    
-    beforeEach(function(){
+
+    beforeEach(function() {
         InventoryKeeper.read = jasmine.createSpy('read');
         PaymentService.add = jasmine.createSpy('add');
         PaymentService.clear = jasmine.createSpy('clear');
         PaymentService.list = jasmine.createSpy('list');
 
     });
-    
+
     beforeEach(inject(function($controller, $rootScope) {
 
         scope = $rootScope.$new();
-        scope.selectPaymentMethod = function(){};
-       
+        scope.selectPaymentMethod = function() {
+        };
+
         $controller('PaymentExchangeCtrl', {
             $scope : scope,
             DialogService : DialogService,
@@ -37,24 +38,28 @@ describe('Controller: PaymentExchangeCtrlConfirm', function() {
 
     it('should add exchanges to payment exchanges', function() {
         // given
-    	scope.exchanges = [];
-    	var exc = {};
-    	exc.amount = 30;
-		exc.qty = 2;
-		exc.amountunit = 15;
-		exc.title = 'test product';
-		exc.id = 1;
-		scope.exchanges.push(exc);
-		
-		var expected = {qty: 2, productId: 1, amount: 30};
-       
+        scope.exchanges = [];
+        var exc = {};
+        exc.amount = 30;
+        exc.qty = 2;
+        exc.amountunit = 15;
+        exc.title = 'test product';
+        exc.id = 1;
+        scope.exchanges.push(exc);
+
+        var expected = {
+            qty : 2,
+            productId : 1,
+            amount : 30
+        };
+
         // when
-		scope.confirmExchangePayments();
+        scope.confirmExchangePayments();
 
         // then
         expect(PaymentService.add).toHaveBeenCalledWith(expected);
     });
-    
+
     it('should clear old exchanges before add confirmed exchanges', function() {
         // given
         scope.exchanges = [];
@@ -65,7 +70,7 @@ describe('Controller: PaymentExchangeCtrlConfirm', function() {
         exc.title = 'test product';
         exc.id = 1;
         scope.exchanges.push(exc);
-       
+
         // when
         scope.confirmExchangePayments();
 
