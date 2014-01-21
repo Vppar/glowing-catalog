322d9b1b214705d61ab28d163986b8500d784cf3
arnaldo.rodrigues <arnaldo.rodrigues@sous.com.br>
2014-01-21 09:34:16 -0200

    Credit card payment.

--- app/scripts/controllers/payment-creditcard.js
+++ app/scripts/controllers/payment-creditcard.js
@@ -2,15 +2,15 @@
     'use strict';
 
     angular.module('tnt.catalog.payment.creditcard', [
-        'tnt.catalog.service.data', 'tnt.catalog.payment.service'
-    ]).controller('PaymentCreditCardCtrl', function($scope, $element, $filter, DataProvider, OrderService) {
+        'tnt.catalog.service.data', 'tnt.catalog.payment.service', 'tnt.catalog.payment.entity'
+    ]).controller('PaymentCreditCardCtrl', function($scope, $element, $filter, DataProvider, OrderService, CreditCardPayment, PaymentService) {
 
         // #####################################################################################################
         // Warm up the controller
         // #####################################################################################################
 
-    	$scope.orderNumber = OrderService.order.code;
-    	
+        $scope.orderNumber = OrderService.order.code;
+
         // Initializing credit card data with a empty credit card
         var creditCard = {};
         var emptyCreditCardTemplate = {
@@ -22,7 +22,8 @@
             number : null,
             cvv : null,
             cardholderName : null,
-            cardholderDocument : null // CPF
+            cardholderDocument : null
+        // CPF
         };
         angular.extend(creditCard, emptyCreditCardTemplate);
         $scope.creditCard = creditCard;
@@ -30,20 +31,21 @@
         // Credit card informations to fill the screen combos.
         $scope.cardFlags = DataProvider.cardData.flags;
         $scope.installments = DataProvider.cardData.installments;
-        
+
         $scope.internet = DataProvider.internet;
         $scope.merchant = DataProvider.gopay.merchant;
-        
+
         $scope.months = DataProvider.date.months;
-        
-        // Creates an array containing year options for credit card expiration dates
+
+        // Creates an array containing year options for credit card expiration
+        // dates
         var currYear = new Date().getFullYear();
         var cardMaxExpirationYear = currYear + 10;
         var cardExpirationYears = [];
         while (currYear < cardMaxExpirationYear) {
-        	cardExpirationYears.push(currYear++);
+            cardExpirationYears.push(currYear++);
         }
-        
+
         $scope.cardExpirationYears = cardExpirationYears;
 
         // Recovering dialogService from parent scope.
@@ -62,15 +64,15 @@
             if (!newCreditCard.amount || newCreditCard.amount === 0) {
                 return;
             }
-            
+
             // check if the all mandatory fields are filed.
             if ($scope.creditCardForm.$valid) {
                 // check if is duplicated.
                 GoPayService.pay(newCreditCard.amount, 'MaryKay pedido 123');
-//                var payment = PaymentService.createNew('creditcard');
-//                payment.amount = newCreditCard.amount;
+                // var payment = PaymentService.createNew('creditcard');
+                // payment.amount = newCreditCard.amount;
                 delete newCreditCard.amount;
-//                payment.data = angular.copy(newCreditCard);
+                // payment.data = angular.copy(newCreditCard);
                 $scope.creditCardForm.$pristine = true;
                 $scope.creditCardForm.$dirty = false;
                 $element.find('input').removeClass('ng-dirty').addClass('ng-pristine');
@@ -85,6 +87,14 @@
         $scope.removeCreditCard = function removeCreditCard(payment) {
             var index = $scope.payments.indexOf(payment);
             $scope.payments.splice(index, 1);
+        };
+
+        $scope.confirmCreditCardPayment = function confirmCreditCardPayment(payment) {
+            if ($scope.creditCardForm.$valid) {
+                console.log('valid');
+            } else {
+                console.log('invalid');
+            }
         };
 
     });
