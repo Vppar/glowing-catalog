8d61346793a8c28b5b3c31ccaa7ba1d9a5de1d60
c4sio <c4ssio@gmail.com>
2014-01-22 22:04:02 -0200

    Adding the entity keeper

---
+++ test/spec/services/EntityKeeper/EntityKeeperAddScenario.js
@@ -0,0 +1,63 @@
+'use strict';
+
+describe('Service: EntityKeeper', function() {
+
+    // load the service's module
+    beforeEach(function() {
+        module('tnt.catalog.entity');
+        module('tnt.catalog.entity.keeper');
+        module('tnt.catalog.entity.entity');
+        module('tnt.catalog.journal');
+        module('tnt.catalog.journal.entity');
+        module('tnt.catalog.journal.replayer');
+    });
+
+    // instantiate service
+    var EntityKeeper = undefined;
+    var Entity = undefined;
+    var JournalEntry = undefined;
+    var scope = null;
+    beforeEach(inject(function(_EntityKeeper_, _Entity_, _JournalEntry_, $rootScope) {
+        EntityKeeper = _EntityKeeper_;
+        Entity = _Entity_;
+        JournalEntry =_JournalEntry_;
+        scope = $rootScope;
+    }));
+    
+    /**
+     * <pre>
+     * @spec EntityKeeper.add#1
+     * Given a valid values
+     * when and create is triggered
+     * then a journal entry must be created
+     * an the entry must be registered
+     * </pre>
+     */
+    it('should add', function() {
+
+      
+        //runs(function() {
+            var id = 1;
+            var name = 'cassiano';
+            var emails = [{address: 'cassiano.tesseroli@gvt.com.br'},{address: 'c4ssio@gmail.com'}];
+            var birthDate = '16/09/1981';
+            var phones = [{ddd: 41, phone: 96491686},{ddd: 41, phone: 30875341}];
+            var cep = '8157170';
+            var document = '1234567890';
+            var addresses = [{street: 'rua', number: 555}, {street: 'rua', number: 556}];
+            var remarks = 'bad client';
+            var ev = new Entity(id, name, emails, birthDate, phones, cep, document, addresses,  remarks);
+            EntityKeeper.create(ev);
+            
+        //});
+      
+        //waitsFor(function() {
+            scope.$apply();
+        //    return EntityKeeper.list().length;
+        //}, "Create failed", 3000);
+      
+        //runs(function() {
+             expect(EntityKeeper.list().length).toBe(1);
+        // });
+    });
+});
