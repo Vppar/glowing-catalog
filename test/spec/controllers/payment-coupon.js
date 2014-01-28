bed4056f59eb4ef1c172e2f5b2d3213581b17621
Mathias Kretschek <mathias@kretschek.com.br>
2014-01-28 11:27:13 -0200

    Re-applied changes from VOPP-194 to match new master state

--- test/spec/controllers/payment-coupon.js
+++ test/spec/controllers/payment-coupon.js
@@ -14,7 +14,7 @@ describe('Controller: PaymentCouponCtrl', function() {
     // load the controller's module
     beforeEach(function() {
         module('tnt.catalog.payment.coupon');
-        module('tnt.catalog.service.order');
+        module('tnt.catalog.order.service');
 
         module(function($provide) {
             $provide.value('$log', log);
