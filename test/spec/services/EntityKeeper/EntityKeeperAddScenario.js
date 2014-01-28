c637d29b07694e51861c9f56be7b9b6aac71f5f9
FABIO-DELL <FABIO-DELL@FABIO-DELL-PC>
2014-01-28 11:07:16 -0200

    add missing test. fix bug on tests.

--- test/spec/services/EntityKeeper/EntityKeeperAddScenario.js
+++ test/spec/services/EntityKeeper/EntityKeeperAddScenario.js
@@ -12,6 +12,7 @@ describe('Service: EntityKeeper', function() {
     // instantiate service
     var EntityKeeper = undefined;
     var Entity = undefined;
+    
     beforeEach(inject(function(_EntityKeeper_, _Entity_) {
         EntityKeeper = _EntityKeeper_;
         Entity = _Entity_;
@@ -26,19 +27,22 @@ describe('Service: EntityKeeper', function() {
      * an the entry must be registered
      * </pre>
      */
-    it('should add', function() {
+    it('should create', function() {
+        //given
+        var id = 1;
+        var name = 'cassiano';
+        var emails = [{address: 'cassiano.tesseroli@gvt.com.br'},{address: 'c4ssio@gmail.com'}];
+        var birthDate = '16/09/1981';
+        var phones = [{ddd: 41, phone: 96491686},{ddd: 41, phone: 30875341}];
+        var cep = '8157170';
+        var document = '1234567890';
+        var addresses = [{street: 'rua', number: 555}, {street: 'rua', number: 556}];
+        var remarks = 'bad client';
+        
+        var ev = new Entity(id, name, emails, birthDate, phones, cep, document, addresses,  remarks);
+        
+        //when
         runs(function(){
-            var id = 1;
-            var name = 'cassiano';
-            var emails = [{address: 'cassiano.tesseroli@gvt.com.br'},{address: 'c4ssio@gmail.com'}];
-            var birthDate = '16/09/1981';
-            var phones = [{ddd: 41, phone: 96491686},{ddd: 41, phone: 30875341}];
-            var cep = '8157170';
-            var document = '1234567890';
-            var addresses = [{street: 'rua', number: 555}, {street: 'rua', number: 556}];
-            var remarks = 'bad client';
-            var ev = new Entity(id, name, emails, birthDate, phones, cep, document, addresses,  remarks);
-            
             EntityKeeper.create(ev);
         });
         
@@ -46,7 +50,9 @@ describe('Service: EntityKeeper', function() {
             return EntityKeeper.list().length;
         }, 'JournalKeeper is taking too long', 300);
         
+        //then
         runs(function(){
+            expect(EntityKeeper.list()[0].name).toBe('cassiano');
             expect(EntityKeeper.list().length).toBe(1);
         });
 
@@ -57,11 +63,11 @@ describe('Service: EntityKeeper', function() {
      * @spec EntityKeeper.add#1
      * Given a invalid values
      * when and create is triggered
-     * then a enitity must not be created
-     * an the entry must be registered
+     * then a exception must be throw 'Wrong instance to EntityKeeper'
      * </pre>
      */
     it('should not add', function() {
+        //given
         var ev = {
             id : 1,
             name : 'cassiano',
@@ -95,9 +101,10 @@ describe('Service: EntityKeeper', function() {
             ],
             remarks : 'bad client'
         };
-        var createCall = function(){EntityKeeper.create(ev);}
-        
-        expect(createCall).toThrow('Wront instance to EntityKeeper');
+        //when
+        var createCall = function(){EntityKeeper.create(ev);};
+        //then
+        expect(createCall).toThrow('Wrong instance to EntityKeeper');
 
     });
 });
