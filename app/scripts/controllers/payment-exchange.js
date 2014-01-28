b50074df6fd5fadc8cecd5125c882482f6795ea0
arnaldo.rodrigues <arnaldo.rodrigues@sous.com.br>
2014-01-28 09:33:46 -0200

    Bug fix on payment exchange screen.

--- app/scripts/controllers/payment-exchange.js
+++ app/scripts/controllers/payment-exchange.js
@@ -26,12 +26,13 @@
         // $scope.products = DataProvider.products;
 
         $scope.products = products;
-        $scope.index = 0;
+        $scope.id = 0;
         $scope.exchanges = [];
 
-        $scope.amounttotal = 0;
-        $scope.qtytotal = 0;
+        $scope.amountTotal = 0;
         $scope.amount = 0;
+        $scope.qtyTotal = 0;
+        $scope.qtyItem = 0;
         $scope.qty = 1;
 
         // #####################################################################################################
@@ -50,18 +51,17 @@
         $scope.add = function add() {
 
             if ($scope.amount !== 0 && $scope.qty > 0) {
-                var product = ArrayUtils.find(products, 'id', Number($scope.productId));
+                var product = angular.copy(ArrayUtils.find(products, 'id', Number($scope.productId)));
 
                 if (product) {
                     var exc = {};
-                    exc.amount = $scope.amount * $scope.qty;
-                    exc.qty = $scope.qty;
-                    exc.amountunit = $scope.amount;
+                    exc.productId = product.id;
                     exc.title = product.title;
                     exc.option = product.option;
-                    $scope.index++;
-                    exc.index = $scope.index;
-                    exc.id = $scope.productId;
+                    exc.price = $scope.amount;
+                    exc.qty = $scope.qty;
+                    exc.amount = $scope.amount * $scope.qty;
+
                     $scope.exchanges.push(exc);
 
                     $scope.price = 0;
@@ -81,7 +81,7 @@
                 btnYes : 'Sim',
                 btnNo : 'Não'
             }).then(function(result) {
-                if(result){
+                if (result) {
                     var index = $scope.exchanges.indexOf(exch);
                     $scope.exchanges.splice(index, 1);
                     $scope.computeTotals();
@@ -93,14 +93,16 @@
         // action.
         // compute grid header
         $scope.computeTotals = function() {
-            $scope.amounttotal = 0;
-            $scope.qtytotal = 0;
-            $scope.id = $scope.exchanges.length;
-            var i = 0;
+            $scope.amountTotal = 0;
+            $scope.qtyItem = $scope.exchanges.length;
+            $scope.qtyTotal = 0;
+            var index = 0;
             for ( var idx in $scope.exchanges) {
-                $scope.amounttotal += $scope.exchanges[idx].amountunit * $scope.exchanges[idx].qty;
-                $scope.qtytotal += $scope.exchanges[idx].qty;
-                $scope.exchanges[idx].index = ++i;
+                var exchange = $scope.exchanges[idx];
+                // rebuild the fake id.
+                exchange.id = ++index;
+                $scope.amountTotal += exchange.amount;
+                $scope.qtyTotal += exchange.qty;
             }
         };
 
@@ -108,23 +110,14 @@
             PaymentService.clear('exchange');
             for ( var ix in $scope.exchanges) {
                 var data = $scope.exchanges[ix];
-                var payment = new ExchangePayment(data.id, data.qty, data.amount);
-                payment.amount = data.amount;
+                var payment = new ExchangePayment(data.id, data.productId, data.qty, data.price, data.amount);
                 PaymentService.add(payment);
             }
             $scope.selectPaymentMethod('none');
         };
 
         // Set already included exchange payments
-        var exchanges = PaymentService.list('exchange');
-        for ( var idx in exchanges) {
-            var item = exchanges[idx];
-            var product = ArrayUtils.find(products, 'id', Number(item.productId));
-            product.amount = item.amount;
-            product.qty = item.qty;
-            product.amountunit = (item.amount / item.qty);
-            $scope.exchanges.push(product);
-        }
+        $scope.exchanges = PaymentService.list('exchange');
         $scope.computeTotals();
 
     });
