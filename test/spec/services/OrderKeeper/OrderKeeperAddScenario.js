c9f47b0466dd41ba7237ceba9473c08662a7aee1
Mathias Kretschek <mathias@kretschek.com.br>
2014-02-10 11:02:16 -0200

    Fixed OrderKeeper scenario tests

--- test/spec/services/OrderKeeper/OrderKeeperAddScenario.js
+++ test/spec/services/OrderKeeper/OrderKeeperAddScenario.js
@@ -2,26 +2,38 @@
 
 describe('Service: OrderKeeperAddScenario', function() {
 
+    var logger = angular.noop;
+
+    var log = {
+        debug : logger,
+        error : logger,
+        warn : logger,
+        fatal : logger
+    };
+
     beforeEach(function() {
         module('tnt.catalog.order');
         module('tnt.catalog.order.keeper');
         module('tnt.catalog.order.entity');
+
+        module(function($provide) {
+          $provide.value('$log', log);
+        });
     });
 
     var OrderKeeper = null;
     var Order = null;
     var $rootScope = null;
+    var JournalKeeper = null;
 
-    beforeEach(inject(function(_OrderKeeper_, _Order_, _$rootScope_, WebSQLDriver) {
-      
-        WebSQLDriver.transaction(function(tx){
-            WebSQLDriver.dropBucket(tx, 'JournalEntry');
-        });
-      
+    beforeEach(inject(function(_OrderKeeper_, _Order_, _$rootScope_, _JournalKeeper_) {
         OrderKeeper = _OrderKeeper_;
         Order = _Order_;
         $rootScope = _$rootScope_;
+        JournalKeeper = _JournalKeeper_;
     }));
+
+    beforeEach(nukeData);
 
     /**
      * <pre>
@@ -32,8 +44,10 @@ describe('Service: OrderKeeperAddScenario', function() {
      * and the entry must be registered
      * </pre>
      */
-    it('should add a order', function() {
+    it('should add an order', function() {
         var ev = null
+        var added = false;
+
         runs(function() {
             //givens
             var uuid = 'cc02b600-5d0b-11e3-96c3-010001000001';
@@ -44,13 +58,24 @@ describe('Service: OrderKeeperAddScenario', function() {
             var items = [];
 
             ev = new Order(uuid, code, canceled, date, customerId, items);
+
             //when
-            OrderKeeper.add(ev);
+            var promise = OrderKeeper.add(ev);
+            promise.then(function (result) {
+                log.debug('Order added!', result);
+                added = true;
+            }, function (err) {
+                log.debug('Failed to add Order!', err);
+            });
+
+            $rootScope.$apply();
         });
 
+
         waitsFor(function() {
-            return OrderKeeper.list().length;
-        }, 'JournalKeeper is taking too long', 300);
+            return added;
+        }, 'OrderKeeper.add()', 300);
+
 
         runs(function() {
             //then
@@ -96,6 +121,21 @@ describe('Service: OrderKeeperAddScenario', function() {
         runs(function(){
             expect(resolution).toBe('Wrong instance to OrderKeeper');
         });
-
     });
+
+
+    function nukeData() {
+        var nuked = null;
+
+        runs(function () {
+            JournalKeeper.nuke().then(function () {
+                log.debug('Nuked data!');
+                nuked = true;
+            });
+        });
+
+        waitsFor(function () {
+            return nuked;
+        }, 'JournalKeeper.nuke()');
+    }
 });
