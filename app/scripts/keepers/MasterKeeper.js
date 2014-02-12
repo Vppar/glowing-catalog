1f1821e00b3d4c7565a25a614ce68e4b5e4ef85c
Fabio Chassot <fabio.chassot@tuntscorp.com>
2014-02-12 17:49:34 -0200

    Created unitary tests for MasterKeeper.

--- app/scripts/keepers/MasterKeeper.js
+++ app/scripts/keepers/MasterKeeper.js
@@ -1,35 +1,37 @@
 (function(angular) {
-  'use strict';
+    'use strict';
 
-  angular.module('tnt.catalog.keeper', ['tnt.catalog.journal']).factory('MasterKeeper', function(JournalEntry) {
+    angular.module('tnt.catalog.keeper', [
+        'tnt.catalog.journal', 'tnt.catalog.journal.entity', 'tnt.catalog.journal.replayer', 'tnt.catalog.journal.keeper'
+    ]).factory('MasterKeeper', function(JournalEntry, JournalKeeper) {
 
-    /**
-     * 
-     * @param {String} eventName The name os the event. I.E. Order
-     * @param {Function} eventType The constructor for the Event Object
-     * @param {Number} eventVersion The version of the event
-     */
-    var MasterKeeper = function(eventName, eventType, eventVersion) {
-      ObjectUtils.ro(this, 'eventName', eventName);
-      ObjectUtils.ro(this, 'eventType', eventType);
-      ObjectUtils.ro(this, 'eventVersion', eventVersion);
-    };
+        /**
+         * 
+         * @param {String} eventDataType The name of the event. I.E. Order
+         * @param {Function} eventType The constructor for the Event Object
+         * @param {Number} eventVersion The version of the event
+         */
+        var MasterKeeper = function(eventDataType, eventType, eventVersion) {
+            ObjectUtils.ro(this, 'eventDataType', eventDataType);
+            ObjectUtils.ro(this, 'eventType', eventType);
+            ObjectUtils.ro(this, 'eventVersion', eventVersion);
+        };
 
-    /**
-     * 
-     * @param {String} eventOp The name of the operation. I.E. Add
-     * @param {Object} eventObj the event object(untyped please!)
-     * @return {Object} the journal entry
-     */
-    ObjectUtils.method(MasterKeeper, 'journalize', function(eventOp, eventObj) {
-      var opName = this.eventName.substr(0, 1) + this.eventName.substr(1) + eventOp;
-      var now = (new Date()).getTime();
-      var event = new this.eventType(eventObj);
-      var entry = new JournalEntry(null, now, opName, this.eventVersion, event);
+        /**
+         * 
+         * @param {String} eventOp The name of the operation. I.E. Add
+         * @param {Object} eventObj the event object(untyped please!)
+         * @return {Object} the journal entry
+         */
+        ObjectUtils.method(MasterKeeper, 'journalize', function(eventOp, eventObj) {
+            var eventName = this.eventDataType.substr(0, 1) + this.eventDataType.substr(1) + eventOp;
+            var now = (new Date()).getTime();
+            var event = new this.eventType(eventObj);
+            var entry = new JournalEntry(null, now, eventName, this.eventVersion, event);
 
-      return JournalKeeper.compose(entry);
+            return JournalKeeper.compose(entry);
+        });
+
+        return MasterKeeper;
     });
-
-    return MasterKeeper;
-  });
 })(angular);
--- test/spec/keepers/MasterKeeper.js
+++ test/spec/keepers/MasterKeeper.js
@@ -1,22 +1,121 @@
 'use strict';
 
 ddescribe('Service: MasterKeeper', function() {
+    
+    var MasterKeeper = null;
+    var Stock = null;
+    var JournalKeeper = {};
+    var JournalEntry = {};
+    var expectedReturn = 'this is sparta';
+    
+    // load the service's module
+    beforeEach(function() {
+        module('tnt.catalog.keeper');
+        module('tnt.catalog.stock.entity');
 
-  // load the service's module
-  beforeEach(module('tnt.catalog.keeper'));
+        JournalKeeper.compose = jasmine.createSpy('JournalKeeper.compose').andReturn(expectedReturn);
 
-  // instantiate service
-  var MasterKeeper = null;
-  beforeEach(inject(function(_MasterKeeper_) {
-    MasterKeeper = _MasterKeeper_;
-  }));
+        module(function($provide) {
+            $provide.value('JournalKeeper', JournalKeeper);
+        });
+    });
 
-  it('should do something', function() {
-    expect(!!MasterKeeper).toBe(true);
-  });
-  
-  it('should initialize the necessary properties on creation');
-  
-  it('should mark the sensitive properties as read only');
+    // instantiate service
+    beforeEach(inject(function(_MasterKeeper_, _Stock_, _JournalEntry_) {
+        MasterKeeper = _MasterKeeper_;
+        Stock = _Stock_;
+        JournalEntry = _JournalEntry_;
+    }));
+
+    it('should be acessible', function() {
+        expect(!!MasterKeeper).toBe(true);
+    });
+
+    describe('when creating a new instance of MasterKeeper', function() {
+        var dataType = null;
+        var eventType = null;
+        var eventVersion = null;
+
+        beforeEach(function() {
+            dataType = 'Order';
+            eventType = 'add';
+            eventVersion = '1';
+        });
+
+        it('should initialize the necessary properties', function() {
+            var masterKeeper = new MasterKeeper(dataType, eventType, eventVersion);
+
+            expect(masterKeeper.eventDataType).toBe(dataType);
+            expect(masterKeeper.eventType).toBe(eventType);
+            expect(masterKeeper.eventVersion).toBe(eventVersion);
+        });
+
+        it('should mark the sensitive properties as read only', function() {
+            var masterKeeper = new MasterKeeper(dataType, eventType, eventVersion);
+
+            var changeEventDataType = function() {
+                masterKeeper.eventDataType = 'Entity';
+            };
+            var changeEventType = function() {
+                masterKeeper.eventType = 'remove';
+            };
+            var changeEventVersion = function() {
+                masterKeeper.eventVersion = '3';
+            };
+
+            expect(changeEventDataType).toThrow();
+            expect(changeEventType).toThrow();
+            expect(changeEventVersion).toThrow();
+
+        });
+    });
+
+    describe('when journalize is triggered', function() {
+        var fakeNow = null;
+        var masterKeeper = null;
+        var dataType = null;
+        var eventVersion = null;
+        var newStock = null;
+        
+        beforeEach(function() {
+            fakeNow = 1412421495;
+            spyOn(Date.prototype, 'getTime').andReturn(fakeNow);
+            dataType = 'Stock';
+            eventVersion = '1';
+            masterKeeper = new MasterKeeper(dataType, Stock, eventVersion);
+            newStock = new Stock(1, 15, 30);
+        });
+
+        it('should create entry with the proper date', function() {
+            masterKeeper.journalize('add', newStock);
+            expect(JournalKeeper.compose.mostRecentCall.args[0].stamp).toEqual(fakeNow);
+        });
+
+        it('should create entry with the proper eventTypeName', function() {
+            masterKeeper.journalize('add', newStock);
+            expect(JournalKeeper.compose.mostRecentCall.args[0].type).toEqual('stockAdd');
+        });
+        
+        it('should create entry with the proper eventVersion', function() {
+            masterKeeper.journalize('add', newStock);
+            expect(JournalKeeper.compose.mostRecentCall.args[0].version).toEqual(eventVersion);
+        });
+        
+        it('should create entry with the proper event', function() {
+            masterKeeper.journalize('add', newStock);
+            expect(JournalKeeper.compose.mostRecentCall.args[0].event).toEqual(newStock);
+        });
+        
+        it('should call JournalKeeper.compose with the proper entry type', function() {
+            var entry = new JournalEntry(null, fakeNow, 'stockAdd', 1, newStock);
+            masterKeeper.journalize('add', newStock);
+            expect(JournalKeeper.compose).toHaveBeenCalledWith(entry);
+        });
+        
+        it('should proxy the return from JorunalKeeper.compose', function() {
+            var result = masterKeeper.journalize('add', newStock);
+            expect(result).toEqual(expectedReturn);
+        });
+    });
 
 });
