1262712d5c44745f55f8bc8f460e206e9bd434bb
wesleyakio <wesleyakio@tuntscorp.com>
2014-02-13 09:53:20 -0200

    Adding userService to manage the login proccess

---
+++ app/scripts/services/UserService.js
@@ -0,0 +1,35 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('tnt.catalog.user', []).service('UserService', function UserService($q) {
+    
+    // FIXME implement criptography
+    
+    /**
+     * @param {String}
+     * @param {String}
+     * @param {Boolean}
+     */
+    this.login = function(user, pass, rememberMe){
+      // TODO log into the driver
+      
+      // FIXME - save the md5
+      localStorage.user = user;
+      
+      var deferred = $q.defer();
+      
+      setTimeout(function(){
+        if(pass === 'marykay'){
+          deferred.resolve();
+        } else {
+          deferred.reject();
+        }
+      }, 1500);
+      
+      return deferred.promise;
+    };
+    this.logout = function(){
+      // TODO log out of the driver
+    };
+  });
+})(angular);
