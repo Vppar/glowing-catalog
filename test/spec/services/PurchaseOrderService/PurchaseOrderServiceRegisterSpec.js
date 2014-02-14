9c6be9246fcda47bc748d81a023601488ac2ffe9
arnaldo.rodrigues <arnaldo.rodrigues@sous.com.br>
2014-02-14 20:27:06 -0200

    Tests fix.

--- test/spec/services/PurchaseOrderService/PurchaseOrderServiceRegisterSpec.js
+++ test/spec/services/PurchaseOrderService/PurchaseOrderServiceRegisterSpec.js
@@ -5,6 +5,7 @@ xdescribe('Service: PurchaseOrderService', function() {
     var PurchaseOrderKeeper = {};
     var ExpenseService = {};
     var PurchaseOrderService = {};
+    var StockService = {};
     var $rootScope = null;
     var $log = {};
     var $q = {};
@@ -37,6 +38,7 @@ xdescribe('Service: PurchaseOrderService', function() {
         module(function($provide) {
             $provide.value('$log', $log);
             $provide.value('PurchaseOrderKeeper', PurchaseOrderKeeper);
+            $provide.value('StockService', StockService);
             $provide.value('ExpenseService', ExpenseService);
         });
     });
