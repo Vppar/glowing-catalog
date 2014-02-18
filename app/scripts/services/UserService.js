6dd6bd6576fc7c0650f3b02b7fca819c5933c54c
muriloquadros <murilo.quadros@tuntscorp.com>
2014-02-18 17:10:42 -0300

    adding login methods at UserServices spec for login methods was created

--- app/scripts/services/UserService.js
+++ app/scripts/services/UserService.js
@@ -1,55 +1,75 @@
 (function(angular) {
     'use strict';
 
-    angular
-    .module('tnt.catalog.user', ['tnt.catalog.sync.driver', 'tnt.catalog.sync.service'])
-    .service('UserService', function UserService($q, $location, SyncDriver, SyncService) {
+    angular.module('tnt.catalog.user', [
+        'angular-md5', 'tnt.catalog.sync.driver', 'tnt.catalog.sync.service'
+    ]).service('UserService', function UserService($q, $location, md5, SyncDriver, SyncService) {
 
-        // FIXME implement criptography
-        
-        //FIXME change default value to FALSE
+        // FIXME change default value to FALSE
         var logged = true;
+        var SALT = '7un7sC0rp';
 
         /**
          * @param {String}
          * @param {String}
          * @param {Boolean}
          */
+        this.loginOnline = function loginOnline(user, pass) {
+            var promise = SyncDriver.login(user, pass);
+            promise.then(function() {
+                logged = true;
+                SyncDriver.registerSyncService(SyncService);
+            });
+
+            return promise;
+        };
+
+        this.loggedIn = function loggedIn(user, pass) {
+            var hashMD5 = md5.createHash(user + ':' + SALT + ':' + pass);
+            localStorage.hashMD5 = hashMD5;
+            return hashMD5;
+        };
+
+        this.loginOffline = function loginOffline(user, pass) {
+            var result = $q.reject();
+            if (localStorage.hashMD5) {
+                var hashMD5 = md5.createHash(user + ':' + SALT + ':' + pass);
+                if (localStorage.hashMD5 === hashMD5) {
+                    result = true;
+                }
+            }
+            return result;
+        };
+
         this.login = function(user, pass, rememberMe) {
-            // FIXME - save the md5
-            localStorage.user = user;
-
-            var promise = SyncDriver.login(user, pass, rememberMe);
-
-            promise.then(function () {
-              logged = true;
-              SyncDriver.registerSyncService(SyncService);
+            var onlineLoggedPromise = this.loginOnline(user, pass);
+            var loggedIn = this.loggedIn;
+            var loginOffline = this.loginOffline;
+            var loggedPromise = onlineLoggedPromise.then(function() {
+                return loggedIn(user, pass);
+            }, function() {
+                return loginOffline(user, pass);
             });
 
-            return promise;
+            return loggedPromise;
         };
-
-
         this.logout = function() {
+            // TODO log out of the driver
             var promise = SyncDriver.logout();
-
-            logged = false;
-
-            promise.then(function () {
-              logged = false;
-            });
             
+            promise.then(function(){
+                logged = false;
+            });
             return promise;
         };
-
         this.isLogged = function isLogged() {
             return logged;
         };
-
         this.redirectIfIsNotLoggedIn = function redirectIfIsNotLoggedIn() {
-          if(!logged) {
-              $location.path('/login');
-          }  
+            if (!logged) {
+                $location.path('/login');
+            }
         };
+
     });
 })(angular);
--- test/spec/services/UserService.js
+++ test/spec/services/UserService.js
@@ -2,48 +2,212 @@
 
 describe('Service: UserService', function() {
 
-    var $log = {
-        debug : angular.noop
-    };
 
-    var SyncServiceMock = {};
-    var SyncDriverMock = {};
-
+    // instantiate service
+    var UserService = null;
+    var $q = {};
+    var scope = {};
+    var md5 = {};
+    var SyncService = {};
+    var SyncDriver = {};
+        
     // load the service's module
     beforeEach(function() {
         module('angular-md5');
         module('tnt.catalog.user');
-
-        module(function ($provide) {
-          $provide.value('SyncService', SyncServiceMock);
-          $provide.value('SyncDriver', SyncDriverMock);
+        module(function($provide){
+            $provide.value('SyncService', SyncService);
+            $provide.value('SyncDriver', SyncDriver);
         });
+        delete localStorage.hashMD5;
+        
+        SyncDriver.login = jasmine.createSpy('SyncDriver.login');
+        SyncDriver.logout = jasmine.createSpy('SyncDriver.logout');
+        
     });
-
-
-    beforeEach(inject(function ($q) {
-        PromiseHelper.config($q, $log.debug);
-    }));
-
-
-    beforeEach(function () {
-        SyncDriverMock.login = jasmine.createSpy('SyncDriver.login');
-        SyncDriverMock.logout = jasmine.createSpy('SyncDriver.logout');
-    });
-
-    // instantiate service
-    var UserService = null;
-
-    beforeEach(inject(function(_UserService_) {
+    
+    beforeEach(inject(function(_UserService_, _$q_, _$rootScope_, _md5_) {
         UserService = _UserService_;
+        $q = _$q_;
+        scope = _$rootScope_;
+        md5 = _md5_;
     }));
 
     it('should instantiate service', function() {
         expect(!!UserService).toBe(true);
     });
 
-    it('should login', function() {
-        SyncDriverMock.login.andCallFake(PromiseHelper.resolved(true));
-        UserService.login();
+    it('should call loggedIn', function() {
+        UserService.loginOnline = jasmine.createSpy('loginOnline').andCallFake(function() {
+            var deferred = $q.defer();
+
+            setTimeout(function() {
+                deferred.resolve();
+            }, 0);
+
+            return deferred.promise;
+        });
+
+        UserService.loggedIn = jasmine.createSpy('loggedIn');
+
+        var user = 'usertest';
+        var pass = 'passtest';
+        var result = null;
+        // when
+        runs(function() {
+            UserService.login(user, pass, false).then(function() {
+                result = true;
+            });
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return result;
+        });
+
+        runs(function() {
+            expect(result).toBe(true);
+            expect(UserService.loggedIn).toHaveBeenCalledWith(user, pass);
+        });
+
     });
+
+    it('should call loginOffline', function() {
+        UserService.loginOnline = jasmine.createSpy('loginOnline').andCallFake(function() {
+            var deferred = $q.defer();
+
+            setTimeout(function() {
+                deferred.reject();
+            }, 0);
+
+            return deferred.promise;
+        });
+
+        UserService.loginOffline = jasmine.createSpy('loginOffline').andCallFake(function() {
+            var deferred = $q.defer();
+
+            setTimeout(function() {
+                deferred.resolve();
+            }, 0);
+
+            return deferred.promise;
+        });
+        ;
+
+        var user = 'usertest';
+        var pass = 'passtest';
+        var result = null;
+        // when
+        runs(function() {
+            UserService.login(user, pass, false).then(function() {
+                result = true;
+            });
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return result;
+        });
+
+        runs(function() {
+            expect(UserService.loginOffline).toHaveBeenCalledWith(user, pass);
+        });
+
+    });
+
+    it('should call loginOffline return rejected promise', function() {
+        UserService.loginOnline = jasmine.createSpy('loginOnline').andCallFake(function() {
+            var deferred = $q.defer();
+
+            setTimeout(function() {
+                deferred.reject();
+            }, 0);
+
+            return deferred.promise;
+        });
+
+        UserService.loginOffline = jasmine.createSpy('loginOffline').andCallFake(function() {
+            var deferred = $q.defer();
+
+            setTimeout(function() {
+                deferred.reject();
+            }, 0);
+
+            return deferred.promise;
+        });
+
+        var user = 'wronguser';
+        var pass = 'wrongpass';
+        var result = null;
+        // when
+        runs(function() {
+            UserService.login(user, pass, false).then(null, function() {
+                result = true;
+            });
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return result;
+        });
+
+        runs(function() {
+            expect(UserService.loginOffline).toHaveBeenCalledWith(user, pass);
+        });
+
+    });
+
+    it('should generate hashMD5', function() {
+
+        var user = 'usertest';
+        var pass = 'passtest';
+        var actual = UserService.loggedIn(user, pass);
+        var SALT = '7un7sC0rp';
+
+        var expected = md5.createHash(user + ':' + SALT + ':' + pass);
+
+        expect(actual).toBe(expected);
+
+    });
+
+    it('should login offline', function() {
+
+        var user = 'usertest';
+        var pass = 'passtest';
+        var SALT = '7un7sC0rp';
+
+        localStorage.hashMD5 = md5.createHash(user + ':' + SALT + ':' + pass);
+
+        var actual = UserService.loginOffline(user, pass);
+
+        expect(actual).toBe(true);
+
+    });
+
+    it('should not login offline', function() {
+
+        localStorage.hashMD5 = md5.createHash('usertest:7un7sC0rp:passtest');
+
+        var user = 'usertest';
+        var pass = 'bla';
+        var actual = UserService.loginOffline(user, pass);
+        var result = null;
+
+        runs(function() {
+            actual.then(null, function() {
+                result = true;
+            });
+        });
+
+        waitsFor(function() {
+            scope.$apply();
+            return result;
+        });
+
+        runs(function() {
+            expect(result).toBe(true);
+        });
+
+    });
+
 });
