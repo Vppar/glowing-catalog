dc34e2a68ab887ecf25c2bd6668aba9be44f91c3
Mathias Kretschek <mathias@kretschek.com.br>
2014-02-18 18:03:35 -0300

    Fixed UserService dependencies tests

--- test/spec/services/UserService.js
+++ test/spec/services/UserService.js
@@ -2,14 +2,38 @@
 
 describe('Service: UserService', function() {
 
+    var $log = {
+        debug : angular.noop
+    };
+
+    var SyncServiceMock = {};
+    var SyncDriverMock = {};
+
     // load the service's module
     beforeEach(function() {
         module('angular-md5');
         module('tnt.catalog.user');
+
+        module(function ($provide) {
+          $provide.value('SyncService', SyncServiceMock);
+          $provide.value('SyncDriver', SyncDriverMock);
+        });
+    });
+
+
+    beforeEach(inject(function ($q) {
+        PromiseHelper.config($q, $log.debug);
+    }));
+
+
+    beforeEach(function () {
+        SyncDriverMock.login = jasmine.createSpy('SyncDriver.login');
+        SyncDriverMock.logout = jasmine.createSpy('SyncDriver.logout');
     });
 
     // instantiate service
     var UserService = null;
+
     beforeEach(inject(function(_UserService_) {
         UserService = _UserService_;
     }));
@@ -19,6 +43,7 @@ describe('Service: UserService', function() {
     });
 
     it('should login', function() {
+        SyncDriverMock.login.andCallFake(PromiseHelper.resolved(true));
         UserService.login();
     });
 });
