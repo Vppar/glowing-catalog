a49337f72ecf8ed7640953a7ad920680c8a3f034
Fabio Chassot <fabio.chassot@tuntscorp.com>
2014-02-19 01:07:35 -0300

    console.log changed to $log.debug

--- app/scripts/services/PrefetchService.js
+++ app/scripts/services/PrefetchService.js
@@ -1,13 +1,13 @@
 (function(angular) {
     'use strict';
 
-    angular.module('tnt.catalog.prefetch.service', []).service('PrefetchService', function PrefetchService($http) {
+    angular.module('tnt.catalog.prefetch.service', []).service('PrefetchService', function PrefetchService($http, $log) {
 
         this.doIt = function() {
             // cache warmUp
             $http.get('wishlist.manifest').success(function(data) {
                 var list = data.split(/\n/);
-                console.log('list is ' + list.length + ' long');
+                $log.debug('list is ' + list.length + ' long');
                 for ( var ix in list) {
                     if (list[ix].indexOf("/") != -1) {
                         $http.get(list[ix]);
