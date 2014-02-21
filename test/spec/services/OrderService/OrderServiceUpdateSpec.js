159d5498ec31e0be8765d037fbd92222db121a3c
muriloquadros <murilo.quadros@tuntscorp.com>
2014-02-21 11:33:30 -0300

    adding update method at OrderService

---
+++ test/spec/services/OrderService/OrderServiceUpdateSpec.js
@@ -0,0 +1,44 @@
+describe('Service: OrderServiceUpdate', function () {
+  var fakeNow = 1386444467895;
+  var logMock = {};
+  var OrderMock = {};
+  var OrderKeeperMock = {};
+  var DataProviderMock = {};
+
+  var OrderService;
+
+  // load the service's module
+  beforeEach(function () {
+
+    module('tnt.catalog.order.service');
+
+    spyOn(Date.prototype, 'getTime').andReturn(fakeNow);
+    logMock.debug = jasmine.createSpy('$log.debug');
+
+    OrderKeeperMock.update = jasmine.createSpy('OrderKeeper.update').andReturn('ok');
+
+    module(function ($provide) {
+      $provide.value('$log', logMock);
+      $provide.value('Order', OrderMock);
+      $provide.value('OrderKeeper', OrderKeeperMock);
+      $provide.value('DataProvider', DataProviderMock);
+    });
+  });
+
+  beforeEach(inject(function(_OrderService_) {
+    OrderService = _OrderService_;
+  }));
+
+
+  /**
+   * OrderService.cancel() works as a proxy to OrderKeeper.cancel(), just
+   * adding some basic error handling. Therefore, it should just invoke
+   * and return the OrderKeeper's results.
+   */
+  it('gets order from OrderKeeper.update()', function () {
+    var order = OrderService.update(1,[{test:'updated'}]);
+    expect(OrderKeeperMock.update).toHaveBeenCalledWith(1,[{test:'updated'}]);
+    // Are we returning the value returned by 'OrderKeeper.cancel()'?
+    expect(order).toEqual('ok');
+  });
+});
