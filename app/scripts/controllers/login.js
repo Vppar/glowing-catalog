3e043fe5aab2d44b1027cc5215b8c4aa08769c09
Mathias Kretschek <mathias@kretschek.com.br>
2014-02-24 15:33:24 -0300

    Updated the warning message in the user change process

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -27,8 +27,8 @@
                     $log.debug('Trying to switch users with unsynced entries.');
                     DialogService.messageDialog({
                         title : 'Atenção! Dados podem ser perdidos!',
-                        message : 'Alguns dados não foram salvos no servidor. '
-                            + 'Ao trocar de usuário estes dados serão excluídos permanentemente. '
+                        message : 'Alguns dados não foram salvos no servidor e serão '
+                            + 'excluídos permanentemente ao trocar de usuário. '
                             + 'Tem certeza de que deseja continuar?',
                         btnYes : 'Sim, tenho certeza.',
                         btnNo : 'Não'
