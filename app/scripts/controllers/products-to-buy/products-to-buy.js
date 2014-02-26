5a75ca19a1bfc16916a85f712a254cb2da7332c8
thiagoclassen <russo.ec@gmail.com>
2014-02-26 14:00:29 -0300

    summary

--- app/scripts/controllers/products-to-buy/products-to-buy.js
+++ app/scripts/controllers/products-to-buy/products-to-buy.js
@@ -2,139 +2,192 @@
     'use strict';
     angular.module('tnt.catalog.productsToBuy.ctrl', [
         'tnt.catalog.stock.service', 'tnt.catalog.timer.service'
-    ]).controller('ProductsToBuyCtrl', function($scope, $log, StockService, PurchaseOrderService, UserService) {
+    ]).controller(
+            'ProductsToBuyCtrl',
+            function($scope, $log, ArrayUtils, StockService, PurchaseOrderService, UserService) {
 
-        UserService.redirectIfIsNotLoggedIn();
-        
-        // #####################################################################################################
-        // Local variables
-        // #####################################################################################################
+                UserService.redirectIfIsNotLoggedIn();
 
-        // #####################################################################################################
-        // Local Functions
-        // #####################################################################################################
+                // #####################################################################################################
+                // Local variables
+                // #####################################################################################################
 
-        function resetWatchedQty() {
-            for ( var ix in $scope.main.stockReport.sessions) {
-                // sessions
-                var session = $scope.main.stockReport.sessions[ix];
-                // lines of that session
-                for ( var ix2 in session.lines) {
-                    // lines
-                    var line = session.lines[ix2];
-                    // items of that line
-                    for ( var ix3 in line.items) {
-                        var item = line.items[ix3];
-                        // backup items to use when a recals is needed
-                        $scope.purchaseOrder.items[item.id] = item;
-                        $scope.purchaseOrder.watchedQty[item.id] = item.qty;
+                // #####################################################################################################
+                // Local Functions
+                // #####################################################################################################
+
+                function resetWatchedQty() {
+                    for ( var ix in $scope.main.stockReport.sessions) {
+                        // sessions
+                        var session = $scope.main.stockReport.sessions[ix];
+                        // lines of that session
+                        for ( var ix2 in session.lines) {
+                            // lines
+                            var line = session.lines[ix2];
+                            // items of that line
+                            for ( var ix3 in line.items) {
+                                var item = line.items[ix3];
+                                // backup items to use when a recals is needed
+                                $scope.purchaseOrder.items[item.id] = item;
+                                $scope.purchaseOrder.watchedQty[item.id] = item.qty;
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-        function loadPurchaseOrders() {
-            $scope.ticket.purchaseOrders = PurchaseOrderService.list();
-        }
+                function loadPurchaseOrders() {
+                    $scope.ticket.purchaseOrders = PurchaseOrderService.list();
+                }
 
-        // #####################################################################################################
-        // Scope variables
-        // #####################################################################################################
-        /**
-         * Shared by all fragments
-         */
-        $scope.main = {};
-        $scope.main.stockReport = StockService.stockReport('all');
-        $scope.tabs = {};
-        $scope.tabs.selected = 'buildOrder';
-
-        /**
-         * Summary tab
-         */
-        $scope.summary = {};
-        $scope.summary.total = {};
-        $scope.summary.discount = {};
-        $scope.summary.freight = {};
-        $scope.summary.total.amount = 0;
-        $scope.summary.total.amount2 = 0;
-        $scope.summary.total.amountWithDiscount = 0;
-        $scope.summary.total.points = 0;
-        $scope.summary.discount.fee = 0;
-        $scope.summary.freight.amount = 0;
-
-        /**
-         * Order tab
-         */
-        $scope.purchaseOrder = {};
-        $scope.purchaseOrder.items = {};
-        $scope.purchaseOrder.watchedQty = {};
-
-        /**
-         * Ticket tab
-         */
-        $scope.ticket = {};
-        $scope.ticket.watchedQty = {};
-        $scope.ticket.checkBox = [];
-        $scope.ticket.selectedPart = 'part1';
-        $scope.ticket.loadPurchaseOrders = loadPurchaseOrders;
-        $scope.ticket.purchaseOrders = [];
-
-        /**
-         * Pending tab
-         */
-        $scope.pending = {};
-
-        // #####################################################################################################
-        // Scope functions
-        // #####################################################################################################
-
-        $scope.selectTab = function selectTab(tabName) {
-            $scope.tabs.selected = tabName;
-            $scope.ticket.selectedPart = 'part1';
-        };
-
-        $scope.summaryIsVisible = function(tabName) {
-            return tabName === 'buildOrder' || tabName === 'confirmOrder';
-        };
-
-        $scope.financialRound = function financialRound(value) {
-            return (Math.round(100 * value) / 100);
-        };
-
-        $scope.resetPurchaseOrder = function resetPurchaseOrder() {
-            setTimeout(function() {
+                // #####################################################################################################
+                // Scope variables
+                // #####################################################################################################
+                /**
+                 * Shared by all fragments
+                 */
+                $scope.main = {};
                 $scope.main.stockReport = StockService.stockReport('all');
-                StockService.updateReport($scope.main.stockReport);
+                $scope.tabs = {};
+                $scope.tabs.selected = 'buildOrder';
+
+                /**
+                 * Summary tab
+                 */
+                $scope.summary = {};
+                $scope.summary.total = {};
+                $scope.summary.discount = {};
+                $scope.summary.freight = {};
+                $scope.summary.total.amount = 0;
+                $scope.summary.total.lines = {};
+                $scope.summary.total.amount2 = 0;
+                $scope.summary.total.sessions = {};
+                $scope.summary.total.amountWithDiscount = 0;
+                $scope.summary.total.points = 0;
+                $scope.summary.discount.fee = 0;
+                $scope.summary.freight.amount = 0;
+
+                /**
+                 * Order tab
+                 */
+                $scope.purchaseOrder = {};
+                $scope.purchaseOrder.items = {};
+                $scope.purchaseOrder.watchedQty = {};
+
+                /**
+                 * Ticket tab
+                 */
+                $scope.ticket = {};
+                $scope.ticket.watchedQty = {};
+                $scope.ticket.checkBox = [];
+                $scope.ticket.selectedPart = 'part1';
+                $scope.ticket.loadPurchaseOrders = loadPurchaseOrders;
+                $scope.ticket.purchaseOrders = [];
+
+                /**
+                 * Pending tab
+                 */
+                $scope.pending = {};
+
+                // #####################################################################################################
+                // Scope functions
+                // #####################################################################################################
+
+                $scope.selectTab = function selectTab(tabName) {
+                    $scope.tabs.selected = tabName;
+                    $scope.ticket.selectedPart = 'part1';
+                };
+
+                $scope.summaryIsVisible = function(tabName) {
+                    return tabName === 'buildOrder' || tabName === 'confirmOrder';
+                };
+
+                $scope.financialRound = function financialRound(value) {
+                    return (Math.round(100 * value) / 100);
+                };
+
+                $scope.resetPurchaseOrder = function resetPurchaseOrder() {
+                    setTimeout(function() {
+                        $scope.main.stockReport = StockService.stockReport('all');
+                        StockService.updateReport($scope.main.stockReport);
+                        resetWatchedQty();
+                    }, 0);
+                };
+
+                // #####################################################################################################
+                // Watchers
+                // #####################################################################################################
+
+                $scope.$watchCollection('purchaseOrder.watchedQty', function(newObj, oldObj) {
+                    var diff = {
+                        amount : 0,
+                        points : 0
+                    };
+
+                    $scope.summary.total.sessions = {};
+                    $scope.summary.total.lines = {};
+
+                    for ( var ix in newObj) {
+
+                        var price = $scope.purchaseOrder.items[ix].price;
+                        var points = $scope.purchaseOrder.items[ix].points;
+                        var session = $scope.purchaseOrder.items[ix].session;
+                        var line = $scope.purchaseOrder.items[ix].line;
+                        var minQty = $scope.purchaseOrder.items[ix].minQty;
+                        var qty = newObj[ix];
+
+                        diff.amount += (newObj[ix] * price);
+                        diff.points += (newObj[ix] * points);
+
+                        if (!$scope.summary.total.sessions[session]) {
+                            $scope.summary.total.sessions[session] = {
+                                total : 0,
+                                minQty : 0,
+                                orderQty : 0,
+                                avg : 0
+                            };
+                        }
+                        if (!$scope.summary.total.lines[line]) {
+                            $scope.summary.total.lines[line] = {
+                                total : 0,
+                                minQty : 0,
+                                orderQty : 0,
+                                avg : 0
+                            };
+                        }
+                        if ((newObj[ix] * price) > 0) {
+                            $scope.summary.total.sessions[session].total += (newObj[ix] * price);
+                            $scope.summary.total.lines[line].total += (newObj[ix] * price);
+                        }
+                        if (minQty) {
+                            $scope.summary.total.sessions[session].minQty += minQty;
+                            $scope.summary.total.lines[line].minQty += minQty;
+                        }
+                        if (qty > 0) {
+                            $scope.summary.total.sessions[session].orderQty += qty;
+                            $scope.summary.total.lines[line].orderQty += qty;
+                        }
+                    }
+
+                    $scope.summary.total.amount = diff.amount;
+                    $scope.summary.total.points = diff.points;
+                    for ( var ix in $scope.summary.total.sessions) {
+                        if ($scope.summary.total.sessions[ix].orderQty > 0) {
+                            $scope.summary.total.sessions[ix].avg =
+                                    ($scope.summary.total.sessions[ix].total) / ($scope.summary.total.sessions[ix].orderQty);
+                        }
+                    }
+                    for ( var ix in $scope.summary.total.lines) {
+                        if ($scope.summary.total.lines[ix].orderQty > 0) {
+                            $scope.summary.total.lines[ix].avg =
+                                    ($scope.summary.total.lines[ix].total) / ($scope.summary.total.lines[ix].orderQty);
+                        }
+                    }
+                });
+
+                // #####################################################################################################
+                // Controller warm up
+                // #####################################################################################################
+
                 resetWatchedQty();
-            }, 0);
-        };
-
-        // #####################################################################################################
-        // Watchers
-        // #####################################################################################################
-
-        $scope.$watchCollection('purchaseOrder.watchedQty', function(newObj, oldObj) {
-            var diff = {
-                amount : 0,
-                points : 0
-            };
-
-            for ( var ix in newObj) {
-                var price = $scope.purchaseOrder.items[ix].price;
-                var points = $scope.purchaseOrder.items[ix].points;
-
-                diff.amount += (newObj[ix] * price);
-                diff.points += (newObj[ix] * points);
-            }
-
-            $scope.summary.total.amount = diff.amount;
-            $scope.summary.total.points = diff.points;
-        });
-
-        // #####################################################################################################
-        // Controller warm up
-        // #####################################################################################################
-
-        resetWatchedQty();
-    });
+            });
 }(angular));
