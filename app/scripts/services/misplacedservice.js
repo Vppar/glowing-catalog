044cab050b4d3df4c8a535b98045f1b8d6e0ed64
Mathias Kretschek <mathias@kretschek.com.br>
2014-03-08 18:01:16 -0300

    Moved discount logic to misplacedservice and implemented its tests

--- app/scripts/services/misplacedservice.js
+++ app/scripts/services/misplacedservice.js
@@ -142,4 +142,321 @@ angular.module('tnt.catalog.misplaced.service', []).service('Misplacedservice', 
         }
       }
     };
+
+    function distributeDiscount(discount, items) {
+      var currTotalDiscount = 0;
+      var largestProductIndex = null;
+      var largestProductValue = 0;
+
+      var total = 0;
+
+    }
+
+
+
+    /**
+     * Defines most of the logic required to handle discounts in the app.
+     */
+    this.discount = (function () {
+
+        /**
+         * Gets the total cost of a single item.
+         * @param {object} item The item for which you want to know the
+         *    total value.
+         * @return {number}
+         * @private
+         */
+        function _getItemTotal(item) {
+            if (!item) { return 0; }
+            var qty = item.qty;
+            var cost = item.cost || item.price || item.amount;
+            return qty * cost;
+        }
+
+        /**
+         * Gets the total cost of a list of items.
+         * @param {Array<object>} items The list of items for which the total
+         *    should be calculated.
+         * @param {function?} getTotal The function used to get the total value
+         *    of each item. This function will be called once for each item.
+         * @return {number}
+         * @private
+         */
+        function _getItemsTotal(items, getTotal) {
+            if (!items) { return 0; }
+
+            getTotal = getTotal || _getItemTotal;
+
+            var total = 0;
+
+            for (var idx in items) {
+                total += getTotal(items[idx]);
+            }
+
+            return total;
+        }
+
+
+        /**
+         * Some float operations create slight innaccuracies in values.
+         * This should normalize them.
+         * @param {number} value The value to normalize.
+         * @return {number} Normalized value.
+         * @private
+         */
+        function _normalizeDecimals(value) {
+            return Math.round(100 * value) / 100;
+        }
+
+
+        /**
+         * Distributes a given value accross all items based on their ratio to
+         * the total value (weighted distribution).
+         * @param {Array<object>} items The items ammong which the value
+         *    should be distributed accross.
+         * @param {number} value The value to be distributed.
+         * @param {function?} getTotal The function used to get the total value
+         *    of each item. This function will be called once for each item.
+         * @return {Array} An array of distributed values (first item is the
+         *    weighted value of the first item, and so on).
+         */
+        function distributeByWeight(items, value, getTotal) {
+            getTotal = getTotal || _getItemTotal;
+
+            var total = _getItemsTotal(items, getTotal);
+            var largestItemIndex = null;
+            var largestItemTotal = 0;
+            var tmpValue = 0;
+
+            var weightedValues = [];
+
+            for (var idx in items) {
+                var item = items[idx];
+
+                var itemTotal = getTotal(item);
+
+                if (largestItemIndex === null || itemTotal > largestItemTotal) {
+                    largestItemIndex = parseInt(idx);
+                    largestItemTotal = itemTotal;
+                }
+
+                var itemRatio = itemTotal / total;
+                var itemValue = _normalizeDecimals(value * itemRatio);
+                var tmpValue = _normalizeDecimals(tmpValue + itemValue);
+
+                weightedValues.push(itemValue);
+
+                var isLastItem = parseInt(idx) === items.length - 1;
+                var valueDoesNotMatch = tmpValue !== value;
+
+                // Since we're rounding the values to the second decimal,
+                // in many cases we'll end up with an inconsistency between the
+                // given total value and the sum of all individual values
+                // of each item. We handle this case by adjusting the value for
+                // the largest item if needed, not allowing the sum of all values
+                // to be greater or lower than the given total value.
+                if (isLastItem && valueDoesNotMatch) {
+                    var valuesDiff = value - tmpValue;
+                    var largestValue = weightedValues[largestItemIndex];
+                    weightedValues[largestItemIndex] = _normalizeDecimals(largestValue + valuesDiff);
+                }
+            }
+
+            return weightedValues;
+        }
+
+
+        /**
+         * Sets the item specific discount for the given item and unsets
+         * its order discount.
+         *
+         * @param {object} item Item to which the specific discount is being
+         *    given.
+         * @param {number} discount The item-specific discount.
+         */
+        function setItemDiscount(item, discount) {
+            if (discount || discount === 0) {
+                item.itemDiscount = discount;
+                unsetOrderDiscount(item);
+            } else {
+                unsetItemDiscount(item);
+            }
+        }
+
+
+        function unsetItemDiscount(item) {
+            delete item.itemDiscount;
+        }
+
+
+        /**
+         * Sets the order discount for the given item and unsets
+         * its item-specific discount.
+         *
+         * @param {object} item Item to which the order discount is being
+         *    given.
+         * @param {number} discount The share of the order discount given to
+         *    the given item.
+         */
+        function setOrderDiscount(item, discount) {
+            if (discount || discount === 0) {
+                item.orderDiscount = discount;
+                unsetItemDiscount(item);
+            } else {
+                unsetOrderDiscount(item);
+            }
+        }
+
+
+        function unsetOrderDiscount(item) {
+            delete item.orderDiscount;
+        }
+
+
+        /**
+         * Distributes the given item-specific discount accross the given
+         * items.
+         *
+         * @param {Array<object>} items Array of items the discount should
+         *    be distributed accross.
+         * @param {number} discount The discount being distributed.
+         */
+        function distributeItemDiscount(items, discount) {
+            var weightedDiscounts = distributeByWeight(items, discount);
+
+            for (var idx in items) {
+                setItemDiscount(items[idx], weightedDiscounts[idx]);
+            }
+        }
+
+
+        /**
+         * Distributes the given order discount accross the given items.
+         * BEWARE!!! This will remove any item-specific discounts from the
+         * given items. It's not this function's responsibility to check
+         * for item-specific items in the list!
+         *
+         * @param {Array<object>} items Array of items the discount should
+         *    be distributed accross.
+         * @param {number} discount The discount being distributed, this will
+         *    probably always be a list containing all items that DO NOT have
+         *    item-specific discounts applied to them.
+         */
+        function distributeOrderDiscount(items, discount) {
+            var weightedDiscounts = distributeByWeight(items, discount);
+
+            for (var idx in items) {
+                setOrderDiscount(items[idx], weightedDiscounts[idx]);
+            }
+        }
+
+
+
+        /**
+         * Gets all items that have an item discount applied to them from
+         * the given list of items.
+         * @param {Array<object>} items List of items to filter.
+         * @return {Array<object>}
+         */
+        function getItemsWithItemDiscount(items) {
+            var itemsWithDiscount = [];
+
+            for (var idx in items) {
+                var item = items[idx];
+                item.itemDiscount && itemsWithDiscount.push(item);
+            }
+
+            return itemsWithDiscount;
+        }
+
+
+        /**
+         * Gets all items that have an order discount applied to them from
+         * the given list of items.
+         * @param {Array<object>} items List of items to filter.
+         * @return {Array<object>}
+         */
+        function getItemsWithOrderDiscount(items) {
+            var itemsWithDiscount = [];
+
+            for (var idx in items) {
+                var item = items[idx];
+                item.orderDiscount && itemsWithDiscount.push(item);
+            }
+
+            return itemsWithDiscount;
+        }
+
+
+        /**
+         * Sums the item discounts of all items in the list.
+         * @param {Array<object>} items List of items to filter.
+         * @return {number} Total item discount from all given items.
+         */
+        function getTotalItemDiscount(items) {
+            var discount = 0;
+
+            for (var idx in items) {
+                discount += items[idx].itemDiscount || 0;
+            }
+
+            return _normalizeDecimals(discount);
+        }
+
+
+        /**
+         * Sums the order discount of all items in the list.
+         * @param {Array<object>} items List of items to filter.
+         * @return {number} Total order discount from all given items.
+         */
+        function getTotalOrderDiscount(items) {
+            var discount = 0;
+
+            for (var idx in items) {
+                discount += items[idx].orderDiscount || 0;
+            }
+
+            return _normalizeDecimals(discount);
+        }
+
+
+        /**
+         * Sums the total discount of all items in the list. This sums
+         * all discounts, both item-specific and order discounts.
+         * @param {Array<object>} items List of items to filter.
+         * @return {number} Total order discount from all given items.
+         */
+        function getTotalDiscount(items) {
+            var discount = 0;
+
+            for (var idx in items) {
+                var item = items[idx];
+                discount += (item.itemDiscount || item.orderDiscount || 0);
+            }
+
+            return _normalizeDecimals(discount);
+        }
+
+
+
+        return {
+            _getItemTotal : _getItemTotal,
+            _getItemsTotal : _getItemsTotal,
+
+            distributeByWeight : distributeByWeight,
+            distributeItemDiscount : distributeItemDiscount,
+            distributeOrderDiscount : distributeOrderDiscount,
+
+            getItemsWithItemDiscount : getItemsWithItemDiscount,
+            getItemsWithOrderDiscount : getItemsWithOrderDiscount,
+
+            getTotalItemDiscount : getTotalItemDiscount,
+            getTotalOrderDiscount : getTotalOrderDiscount,
+            getTotalDiscount : getTotalDiscount,
+
+            setItemDiscount : setItemDiscount,
+            setOrderDiscount : setOrderDiscount
+        };
+    })();
+
 });
---
+++ test/spec/services/Misplacedservice/MisplacedserviceDiscount.js
@@ -0,0 +1,382 @@
+'use strict';
+
+describe('Misplacedservice.discount', function() {
+
+    // load the service's module
+    beforeEach(module('tnt.catalog.misplaced.service'));
+
+    // instantiate service
+    var Misplacedservice = null;
+    var Discount = null;
+
+    var item1, item2, item3, item4, items = null;
+
+    beforeEach(inject(function(_Misplacedservice_) {
+        Misplacedservice = _Misplacedservice_;
+        Discount = Misplacedservice.discount;
+
+        item1 = {qty : 1, cost : 10};
+        item2 = {qty : 2, cost : 10};
+        item3 = {qty : 3, cost : 10};
+        item4 = {qty : 4, cost : 10};
+
+        items = [item1, item2, item3, item4];
+    }));
+
+
+    it('is accessible', function () {
+        expect(Discount).not.toBeUndefined();
+    });
+
+
+    it('is an object', function () {
+        expect(typeof Discount).toBe('object');
+    });
+
+
+    describe('.distributeByWeight()', function () {
+        it('is accessible', function () {
+            expect(Discount.distributeByWeight).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.distributeByWeight).toBe('function');
+        });
+
+        it('returns an array of numbers', function () {
+            var distributedValues = Discount.distributeByWeight(items, 50);
+
+            for (var idx in distributedValues) {
+                expect(typeof distributedValues[idx]).toBe('number');
+            }
+        });
+
+        // Check that the returned values have the same ratio to the
+        // given value as the item's value has to the sum of all items.
+        it('returns weighted values', function () {
+            var distributedValues = Discount.distributeByWeight(items, 50);
+
+            for (var idx in distributedValues) {
+                expect(distributedValues[idx]).toBe(5 * (parseInt(idx) + 1));
+                expect(typeof distributedValues[idx]).toBe('number');
+            }
+        });
+
+
+        it('keeps the same order of the items', function () {
+            var distributedValues = Discount.distributeByWeight(items, 50);
+            expect(distributedValues).toEqual([5, 10, 15, 20]);
+        });
+
+        // Check that, if an innacuracy happens due to the rounding of the
+        // distributed values, this innacuracy should be fixed by adjusting
+        // the value of the item with the largest value in list.
+        it('prevents rounding innacuracy by adjusting the item with largest value',
+          function () {
+              item2.qty = 3;
+              item3.qty = 6;
+              item4.qty = 1;
+
+              // Since we're rounding each item's value to the second decimal,
+              // in many cases we'll end up with an inconsistency between the
+              // given total discount and the sum of all individual discounts
+              // of each item. We handle this case by adjusting the value for
+              // the last item if needed, not allowing the sum of all discounts
+              // to be greater than the given total discount.
+              var distributedValues = Discount.distributeByWeight(items, 50);
+
+              var summedDiscount = 0;
+              for (var idx in distributedValues) {
+                  summedDiscount += distributedValues[idx];
+              }
+
+              // If no adjustment was made, we would expect the total to be
+              // 50.01 and the third item would have it's original value of
+              // 27.27 (it receives the adjustment since it has the largest
+              // value).
+              expect(summedDiscount).toBe(50);
+              expect(distributedValues[2]).toBe(27.26);
+          });
+    }); // .distributeByWeight()
+
+
+    describe('.distributeItemDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.distributeItemDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.distributeItemDiscount).toBe('function');
+        });
+
+        it('sets the item discount attr for each item', function () {
+            Discount.distributeItemDiscount(items, 50);
+            for (var idx in items) {
+                expect(items[idx].itemDiscount).toBe(5 * (parseInt(idx) + 1));
+            }
+        });
+    }); // .distributeItemDiscount()
+
+
+    describe('.distributeOrderDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.distributeOrderDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.distributeOrderDiscount).toBe('function');
+        });
+
+        it('sets the order discount attr for each item', function () {
+            Discount.distributeOrderDiscount(items, 50);
+            for (var idx in items) {
+                expect(items[idx].orderDiscount).toBe(5 * (parseInt(idx) + 1));
+            }
+        });
+    }); // .distributeOrderDiscount()
+
+
+    describe('.getItemsWithItemDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.getItemsWithItemDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.getItemsWithItemDiscount).toBe('function');
+        });
+
+        it('returns an array', function () {
+            item1.itemDiscount = 1;
+            item2.itemDiscount = 2;
+
+            var itemsWithDiscount = Discount.getItemsWithItemDiscount(items);
+            expect(Array.isArray(itemsWithDiscount)).toBe(true);
+        });
+
+        it('returns an empty array if there are no items with item discount',
+          function () {
+              var itemsWithDiscount = Discount.getItemsWithItemDiscount(items);
+              expect(itemsWithDiscount).toEqual([]);
+          });
+
+        it('does not include items with 0 item discount', function () {
+            item1.itemDiscount = 0;
+            var itemsWithDiscount = Discount.getItemsWithItemDiscount(items);
+            expect(itemsWithDiscount).toEqual([]);
+        });
+
+        it('only returns items with item discount', function () {
+            item1.itemDiscount = 0;
+            item2.itemDiscount = 10;
+            item4.itemDiscount = 5;
+
+            var itemsWithDiscount = Discount.getItemsWithItemDiscount(items);
+            expect(itemsWithDiscount).toEqual([item2, item4]);
+        });
+    }); // .getItemsWithItemDiscount()
+
+
+    describe('.getItemsWithOrderDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.getItemsWithOrderDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.getItemsWithOrderDiscount).toBe('function');
+        });
+
+        it('returns an array', function () {
+            item1.orderDiscount = 1;
+            item2.orderDiscount = 2;
+
+            var itemsWithDiscount = Discount.getItemsWithOrderDiscount(items);
+            expect(Array.isArray(itemsWithDiscount)).toBe(true);
+        });
+
+        it('returns an empty array if there are no items with order discount',
+          function () {
+              var itemsWithDiscount = Discount.getItemsWithOrderDiscount(items);
+              expect(itemsWithDiscount).toEqual([]);
+          });
+
+        it('does not include items with 0 order discount', function () {
+            item1.orderDiscount = 0;
+            var itemsWithDiscount = Discount.getItemsWithOrderDiscount(items);
+            expect(itemsWithDiscount).toEqual([]);
+        });
+
+        it('only returns items with order discount', function () {
+            item1.orderDiscount = 0;
+            item2.orderDiscount = 10;
+            item4.orderDiscount = 5;
+
+            var itemsWithDiscount = Discount.getItemsWithOrderDiscount(items);
+            expect(itemsWithDiscount).toEqual([item2, item4]);
+        });
+    }); // .getItemsWithOrderDiscount()
+
+
+    describe('.getTotalItemDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.getTotalItemDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.getTotalItemDiscount).toBe('function');
+        });
+
+        it('returns a number', function () {
+            var totalItemDiscount = Discount.getTotalItemDiscount(items);
+            expect(typeof totalItemDiscount).toBe('number');
+        });
+
+        it('sums the value of all item discounts of the items in the list',
+          function () {
+              item1.itemDiscount = 3;
+              item4.itemDiscount = 6;
+              var totalItemDiscount = Discount.getTotalItemDiscount(items);
+              expect(totalItemDiscount).toBe(9);
+          });
+
+        it('returns 0 if there are no items with item discount', function () {
+            var totalItemDiscount = Discount.getTotalItemDiscount(items);
+            expect(totalItemDiscount).toBe(0);
+        });
+    }); // .getTotalItemDiscount()
+
+
+    describe('.getTotalOrderDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.getTotalOrderDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.getTotalOrderDiscount).toBe('function');
+        });
+
+        it('returns a number', function () {
+            var totalItemDiscount = Discount.getTotalOrderDiscount(items);
+            expect(typeof totalItemDiscount).toBe('number');
+        });
+
+        it('sums the value of all order discounts of the items in the list',
+          function () {
+              item1.orderDiscount = 3;
+              item4.orderDiscount = 6;
+              var totalItemDiscount = Discount.getTotalOrderDiscount(items);
+              expect(totalItemDiscount).toBe(9);
+          });
+
+        it('returns 0 if there are no items with order discount', function () {
+            var totalItemDiscount = Discount.getTotalOrderDiscount(items);
+            expect(totalItemDiscount).toBe(0);
+        });
+    }); // .getTotalOrderDiscount()
+
+
+
+    describe('.getTotalDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.getTotalDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.getTotalDiscount).toBe('function');
+        });
+
+        it('returns a number', function () {
+            var totalItemDiscount = Discount.getTotalDiscount(items);
+            expect(typeof totalItemDiscount).toBe('number');
+        });
+
+        // Check that it includes both item discounts and order discounts
+        it('sums the value of all discounts of the items in the list', function () {
+            item1.itemDiscount = 3;
+            item4.orderDiscount = 6;
+            var totalItemDiscount = Discount.getTotalDiscount(items);
+            expect(totalItemDiscount).toBe(9);
+        });
+
+        it('returns 0 if there are no items with discount', function () {
+            var totalItemDiscount = Discount.getTotalDiscount(items);
+            expect(totalItemDiscount).toBe(0);
+        });
+    }); // .getTotalDiscount()
+
+
+
+    describe('.setItemDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.setItemDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.setItemDiscount).toBe('function');
+        });
+
+        it('sets the item discount to the given value', function () {
+            Discount.setItemDiscount(item1, 10);
+            expect(item1.itemDiscount).toBe(10);
+        });
+
+        it('deletes the item discount attribute if set to null or undefined',
+            function () {
+                Discount.setItemDiscount(item1, 10);
+                Discount.setItemDiscount(item2, 10);
+                expect(item1.itemDiscount).toBe(10);
+                expect(item2.itemDiscount).toBe(10);
+
+                Discount.setItemDiscount(item1, null);
+                Discount.setItemDiscount(item2);
+                expect(item1.itemDiscount).toBeUndefined();
+                expect(item2.itemDiscount).toBeUndefined();
+            });
+
+        it('removes order discount from the item if an item discount is set',
+          function () {
+              Discount.setOrderDiscount(item1, 10);
+              Discount.setItemDiscount(item1, 5);
+              expect(item1.orderDiscount).toBeUndefined();
+              expect(item1.itemDiscount).toBe(5);
+          });
+    }); // .setItemDiscount()
+
+
+
+    describe('.setOrderDiscount()', function () {
+        it('is accessible', function () {
+            expect(Discount.setOrderDiscount).not.toBeUndefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof Discount.setOrderDiscount).toBe('function');
+        });
+
+        it('sets the order discount to the given value', function () {
+            Discount.setOrderDiscount(item1, 10);
+            expect(item1.orderDiscount).toBe(10);
+        });
+
+        it('deletes the order discount attribute if set to null or undefined',
+            function () {
+                Discount.setOrderDiscount(item1, 10);
+                Discount.setOrderDiscount(item2, 10);
+                expect(item1.orderDiscount).toBe(10);
+                expect(item2.orderDiscount).toBe(10);
+
+                Discount.setOrderDiscount(item1, null);
+                Discount.setOrderDiscount(item2);
+                expect(item1.orderDiscount).toBeUndefined();
+                expect(item2.orderDiscount).toBeUndefined();
+            });
+
+        it('removes item discount from the item if an order discount is set',
+          function () {
+              Discount.setItemDiscount(item1, 10);
+              Discount.setOrderDiscount(item1, 5);
+              expect(item1.itemDiscount).toBeUndefined();
+              expect(item1.orderDiscount).toBe(5);
+          });
+    }); // .setItemDiscount()
+
+});
