65fc17b53429e98372ff4613c517b8eb81d62d99
muriloquadros <murilo.quadros@tuntscorp.com>
2014-03-11 11:53:05 -0300

    improve tests coverage

--- test/spec/controllers/products-to-buy/products-to-buy-persist-purchase-order-spec.js
+++ test/spec/controllers/products-to-buy/products-to-buy-persist-purchase-order-spec.js
@@ -1,11 +1,34 @@
 describe('Controller: products-to-buy-confirm-spec', function() {
 
     var PurchaseOrderService = {};
-
+    var item1 = null;
+    var item2 = null;
+    var item3 = null;
+    var item4 = null;
+    
     // load the controller's module
     beforeEach(function() {
         module('tnt.catalog.productsToBuy.confirm.ctrl');
         module('tnt.catalog.stock.entity');
+    });
+    
+    beforeEach(function() {
+        item1 = {
+            id : 1,
+            qty : 0
+        };
+        item2 = {
+            id : 2,
+            qty : 4
+        };
+        item3 = {
+            id : 3,
+            qty : 0
+        };
+        item4 = {
+            id : 4,
+            qty : 2
+        };
     });
 
 
@@ -13,13 +36,33 @@ describe('Controller: products-to-buy-confirm-spec', function() {
     beforeEach(inject(function($q, $controller, $rootScope, _$filter_) {
         scope = $rootScope.$new();
 
-        scope.main = {};
-        scope.main.stockReport = {};
         scope.summary = {};
         scope.summary.total = {};
         scope.summary.total.amount = 0;
         scope.summary.total.amountWithDiscount = 0;
         scope.financialRound = function(a){};
+        
+        scope.purchaseOrder = {};
+        scope.purchaseOrder.watchedQty = {
+            '1' : 0,
+            '2' : 4,
+            '3' : 0,
+            '4' : 2
+        };
+        scope.main = {};
+        scope.main.stockReport = {
+            sessions : {
+                mySession1 : {
+                    lines : {
+                        myLine1 : {
+                            items : [
+                                item1, item2, item3, item4
+                            ]
+                        }
+                    }
+                }
+            }
+        };
         
         productsToBuyConfirmCtrl = $controller('ProductsToBuyConfirmCtrl', {
             $scope : scope,
@@ -31,6 +74,8 @@ describe('Controller: products-to-buy-confirm-spec', function() {
     }));
 
     it('should confirm products', function() {
+        
+       
 
         scope.persitPurchaseOrder();
         
