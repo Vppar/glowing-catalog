63d5b2c3031a440e934274916c587b594f4c84ef
Mathias Kretschek <mathias@kretschek.com.br>
2014-03-12 10:32:40 -0300

    Ultra-basic password safety check and improved message handling

--- app/scripts/controllers/change-pass-dialog.js
+++ app/scripts/controllers/change-pass-dialog.js
@@ -3,21 +3,60 @@
 
     angular.module('glowingCatalogApp').controller(
         'ChangePassDialogCtrl',
-        ['$scope', 'dialog', 'UserService', 'DialogService',
-        function($scope, dialog, UserService, DialogService) {
+        ['$scope', '$log', 'dialog', 'UserService', 'DialogService',
+        function($scope, $log, dialog, UserService, DialogService) {
 
+
+            /** Closes the change password dialog. */
             function closeDialog() {
                 dialog.close();
             }
 
 
-
+            // FIXME review these messages
             /**
+             * Contains messages ready to be displayed to the end user.
+             * This messages should have an id, which usually is an error
+             * code received from Firebase.
              * @enum {String}
              */
-            var ChangePasswordError = {
-                INVALID_PASSWORD : 'INVALID_PASSWORD'
+            var Message = {
+                /** The given password was not accepted. */
+                INVALID_PASSWORD : 'Senha inválida.',
+
+                /** Not connected to Firebase. */
+                NOT_CONNECTED : 'Dispositivo não conectado à internet.',
+
+                /** The given password is not safe enough. */
+                UNSAFE_PASSWORD : 'Senhas devem conter pelo menos 6 caracteres.',
+
+                /** Default error. */
+                UNEXPECTED_ERROR : 'Houve um erro não esperado. Por favor, tente novamente.'
             };
+
+
+            /**
+             * Gets a proper error message for the given error.
+             * @param {Object|string} err The error for which we need a message
+             * @return {string} A proper error message.
+             */
+            function getMessageForError(err) {
+                if (err) {
+                    var errId = err.code || err.message || err;
+
+                    switch (errId) {
+                        case 'INVALID_PASSWORD':
+                            return Message.INVALID_PASSWORD;
+                        case 'Not connected to Firebase!':
+                            return Message.NOT_CONNECTED;
+                        case 'Password not safe enough':
+                            return Message.UNSAFE_PASSWORD;
+                    }
+                }
+
+                $log.debug('Unexpected error:', err);
+                return Message.UNEXPECTED_ERROR;
+            }
 
 
             function changePassword() {
@@ -36,22 +75,11 @@
                             closeDialog();
                         });
                     }, function (err) {
-                        err = err.code || err.message || err;
-
-                        // FIXME need to check possible error messages and
-                        // create a user-friendly one.
-
-                        var message = '';
-
-                        switch (err) {
-                            case ChangePasswordError.INVALID_PASSWORD:
-                                message = 'Senha antiga inválida!';
-                                break;
-                        }
-
+                        // err might come from the UserService or all the way
+                        // from the SyncDriver.
                         DialogService.messageDialog({
                             title : 'Troca de senha',
-                            message : 'Não foi possível alterar sua senha. ' + message,
+                            message : getMessageForError(err),
                             btnYes : 'OK'
                         });
                     });
