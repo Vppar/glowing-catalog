e5cd6e5b0004482a07c14aec98695668106d62fe
arnaldorodrigues <arnaldorodrigues@tuntscorp.com>
2014-03-12 02:36:25 -0300

    - Fast click applied to datapicker and confirm/cancel payment buttons.

--- app/scripts/preloader/datepicker.js
+++ app/scripts/preloader/datepicker.js
@@ -1,6 +1,6 @@
 angular.module("template/datepicker/popup.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/datepicker/popup.html",
-    "<ul class=\"dropdown-menu\" ng-style=\"{display: (isOpen && 'block') || 'none', top: position.top+'px', left: position.left+'px'}\" class=\"dropdown-menu\">\n" +
+    "<ul class=\"dropdown-menu\" fast-click ng-style=\"{display: (isOpen && 'block') || 'none', top: position.top+'px', left: position.left+'px'}\" class=\"dropdown-menu\">\n" +
     "   <li ng-transclude></li>\n" +
     "   <li class=\"divider\"></li>\n" +
     "   <li style=\"padding: 9px;\">\n" +
