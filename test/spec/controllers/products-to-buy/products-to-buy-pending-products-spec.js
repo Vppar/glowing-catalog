17334237857aeb751c84bc320c9a8eeef0891506
muriloquadros <murilo.quadros@tuntscorp.com>
2014-03-13 09:32:38 -0300

    tests for products-to-buy-pending-products were implemented

--- test/spec/controllers/products-to-buy/products-to-buy-pending-products-spec.js
+++ test/spec/controllers/products-to-buy/products-to-buy-pending-products-spec.js
@@ -1,4 +1,4 @@
-ddescribe('Controller: products-to-buy-confirm-list-spec', function() {
+describe('Controller: products-to-buy-confirm-list-spec', function() {
 
     var PurchaseOrderService = {};
 
@@ -8,20 +8,20 @@ ddescribe('Controller: products-to-buy-confirm-list-spec', function() {
     });
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function($controller, $rootScope, _$filter_) {
+    beforeEach(inject(function($controller, $rootScope) {
         scope = $rootScope.$new();
+        scope.pending = {};
+        PurchaseOrderService.listPendingPurchaseOrders = jasmine.createSpy('PurchaseOrderService.listPendingPurchaseOrders');
 
         productsToBuyPendingCtrl = $controller('ProductsToBuyPendingCtrl', {
             $scope : scope,
             PurchaseOrderService : PurchaseOrderService
         });
-        
     }));
 
     it('should list pending products', function() {
-        PurchaseOrderService.listPendingPurchaseOrders = jasmine.createSpy('PurchaseOrderService.listPendingPurchaseOrders');
         expect(PurchaseOrderService.listPendingPurchaseOrders).toHaveBeenCalled();
-        
+        expect(scope.watchedQty).toEqual({});
     });
 
 });
