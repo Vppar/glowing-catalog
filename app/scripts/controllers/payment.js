35e605055eea56088deb2d5616f6b53bfa6ee40d
muriloquadros <murilo.quadros@tuntscorp.com>
2014-03-17 10:51:35 -0300

    fixing voucher warning

--- app/scripts/controllers/payment.js
+++ app/scripts/controllers/payment.js
@@ -576,9 +576,25 @@
 
                             function updateCoupons() {
                                 var vouchers = ArrayUtils.list(VoucherKeeper.list('voucher'), 'entity', order.customerId);
+                                vouchers = getAvailableCouponArray(vouchers);
+                                console.log('vouchers',vouchers);
                                 var giftCard = ArrayUtils.list(VoucherKeeper.list('giftCard'), 'entity', order.customerId);
+                                giftCard = getAvailableCouponArray(giftCard);
+                                console.log('giftCard', giftCard);
                                 var coupon = ArrayUtils.list(VoucherKeeper.list('coupon'), 'entity', order.customerId);
+                                coupon = getAvailableCouponArray(coupon);
+                                console.log('coupon',coupon);
                                 $scope.hasCoupons = vouchers.length > 0 || giftCard.length > 0 || coupon.length > 0;
+                            }
+                            
+                            function getAvailableCouponArray(array) {
+                                var newArray = [];
+                                for (var ix in array){
+                                    if(!array[ix].redeemed && !array[ix].canceled) {
+                                        newArray.push(array[ix]);
+                                    }
+                                }
+                                return newArray;
                             }
 
                             // #############################################################################################
