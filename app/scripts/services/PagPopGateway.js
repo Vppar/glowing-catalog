2a63e8ab5e539f2f8d8b385bc942dff55e755334
arnaldorodrigues <arnaldo.rodrigues@tuntscorp.com>
2014-03-24 02:09:12 -0300

    Response adjust.

--- app/scripts/services/PagPopGateway.js
+++ app/scripts/services/PagPopGateway.js
@@ -3,9 +3,9 @@
 
     angular.module('tnt.catalog.pagpop.gateway', []).service('PagPopGateway', [
         '$q', '$http', 'logger', function PagPopGateway($q, $http, logger) {
-            
+
             var log = logger.getLogger('tnt.catalog.pagpop.gateway.PagPopGateway');
-            
+
             this.pay = function(data) {
                 var deferred = $q.defer();
                 var token = localStorage.ppToken;
@@ -14,19 +14,20 @@
                     url : 'https://vopp.com.br/pagpop/card/?token=' + token,
                     data : data,
                 }).success(function(data) {
-                    if (data.Status !== 0) {
-                        deferred.reject(data);
-                    } else {
+                    if (data && data.transacao && data.transacao.codigo_retorno === '0') {
+                        log.info('Credit card payment processed.', data);
                         deferred.resolve(data);
+                    } else {
+                        log.info('An error has occour while processing a credit card payment.', data);
+                        deferred.reject(data);
                     }
                 }).error(function(result) {
-                    log.fatal('PapPopGateway.pay: An error has occour while processing a credit card payment.', result);
+                    log.fatal('There was an error contacting the server', result);
                     deferred.reject({
                         Status : 'conn',
                         Message : 'There was an error contacting the server'
                     });
                 });
-
                 return deferred.promise;
             };
         }
