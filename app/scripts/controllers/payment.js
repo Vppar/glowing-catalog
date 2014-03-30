e87b7b579ac7c00838e786bc31474b33a1d1564a
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2014-03-30 18:57:01 -0300

    Only suggest if hasn't change.

--- app/scripts/controllers/payment.js
+++ app/scripts/controllers/payment.js
@@ -345,8 +345,8 @@
 
                             $scope.selectMoneyPayment = function selectMoneyPayment() {
                                 // FIXME - Used to temporally resolve VOPP-210.
-                                if(getCashAmount() === 0) {
-                                    $scope.cash.amount = angular.copy(total.change);
+                                if(getCashAmount() === 0 && $scope.total.change < 0) {
+                                    $scope.cash.amount = angular.copy($scope.total.change);
                                 }
                                 var delta = new Date().getTime() - $scope.keyboard.status.changed;
                                 delta = isNaN(delta) ? 501 : delta;
