5aafe0c7aba76fc4580169a0afc64a0d80d3055c
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2014-04-04 21:08:13 -0300

    Distributing spinner button through the app.

--- app/scripts/services/PagPopGateway.js
+++ app/scripts/services/PagPopGateway.js
@@ -1,4 +1,4 @@
-(function(angular) {
+(function (angular) {
     'use strict';
 
     angular.module('tnt.catalog.pagpop.gateway', []).service('PagPopGateway', [
@@ -6,43 +6,58 @@
 
             var log = logger.getLogger('tnt.catalog.pagpop.gateway.PagPopGateway');
 
-            this.pay = function(data) {
+            this.pay = function (data) {
                 var deferred = $q.defer();
                 var token = localStorage.ppToken;
                 $http({
-                    method : 'POST',
-                    url : 'https://vopp.com.br/pagpop/card/?token=' + token,
-                    data : data,
-                }).success(function(data) {
-                    if (data && data.transacao && 
-                            (data.transacao.situacao === 'demonstrativo' || data.transacao.situacao === 'aprovado')) {
+                    method: 'POST',
+                    url: 'https://vopp.com.br/pagpop/card/?token=' + token,
+                    data: data
+                }).success(function (data) {
+                    if (data && data.transacao &&
+                        (data.transacao.situacao === 'demonstrativo' || data.transacao.situacao === 'aprovado')) {
                         log.info('Credit card payment processed.', data);
                         deferred.resolve(data);
                     } else {
-                        log.fatal('An error has occour while processing a credit card payment.', data);
+                        log.fatal('Transaction rejected.', data);
                         deferred.reject({
-                            status : 'conn',
-                            message : 'There was an error contacting the server.'
+                            status: 'rejected',
+                            message: 'Transaction rejected.'
                         });
                     }
-                }).error(function(err) {
-                    var rejection = null;
-                    if (err && err.errors && err.errors.bandeira) {
-                        rejection = {
-                            status : '-3',
-                            message : 'Invalid credit card number'
-                        };
-                    } else {
-                        rejection = {
-                            status : 'conn',
-                            message : 'There was an error contacting the server.'
-                        };
+                }).error(function (err) {
+                        var rejection = null;
+                        if (err && err.errors) {
+                            if (err.errors.bandeira) {
+                                rejection = {
+                                    status: 'invalidCard',
+                                    message: 'Invalid credit card number'
+                                };
+                            } else if (err.errors.base) {
+                                rejection = {
+                                    status: 'minAmount',
+                                    message: 'Transaction rejected.'
+                                };
+                            } else {
+                                rejection = {
+                                    status: 'rejected',
+                                    message: 'Transaction rejected.'
+                                };
+                            }
+                        } else {
+                            rejection = {
+                                status: 'conn',
+                                message: 'There was an error contacting the server.'
+                            };
+                        }
+                        log.fatal('An error occur while processing a credit card payment', err);
+                        deferred.reject(rejection);
                     }
-                    log.fatal('An error occur while processing a credit card payment', err);
-                    deferred.reject(rejection);
-                });
+                )
+                ;
                 return deferred.promise;
             };
         }
-    ]);
+    ])
+    ;
 }(angular));
