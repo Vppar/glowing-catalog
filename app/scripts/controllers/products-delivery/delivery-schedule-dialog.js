14710cce19310eebbac4ba24f902fe4d40c6a3d8
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2014-04-17 19:13:07 -0300

    Fixing mask.

--- app/scripts/controllers/products-delivery/delivery-schedule-dialog.js
+++ app/scripts/controllers/products-delivery/delivery-schedule-dialog.js
@@ -14,27 +14,27 @@
             function ($scope, $q, logger, dialog, SchedulingService) {
                 var log = logger.getLogger('tnt.catalog.productsDeliverySchedule');
                 $scope.scheduledDelivery = {
-                    orderUUID : dialog.data.orderUUID,
-                    date : dialog.data.deliveryDate,
-                    items : dialog.data.items,
-                    disabled : true
+                    orderUUID: dialog.data.orderUUID,
+                    date: dialog.data.deliveryDate,
+                    items: dialog.data.items,
+                    disabled: true
                 };
-                
+
                 $scope.minDate = new Date();
-                $scope.minDate.setDate($scope.minDate.getDate() +1); 
-                
+                $scope.minDate.setDate($scope.minDate.getDate() + 1);
+
                 $scope.schedule = {
-                    hour : '08',
-                    minute : '00'
+                    hour: '08',
+                    minute: '00'
                 };
-                
-                $scope.cancel = function cancel () {
+
+                $scope.cancel = function cancel() {
                     dialog.close($q.reject());
                     return $q.reject('Canceled');
                 };
 
                 $scope.confirm =
-                    function confirm () {
+                    function confirm() {
                         // schedule
                         var schedule = SchedulingService.readByDocument($scope.scheduledDelivery.orderUUID);
                         if (schedule) {
@@ -75,37 +75,49 @@
 
                     };
 
+                function zeroFill(target) {
+                    var result = target;
+                    if (target.length < 2) {
+                        result = '0' + target;
+                    } else if (target.length > 2) {
+                        result = target.slice(1,3);
+                    }
+                    return result;
+                }
+
                 $scope.$watchCollection('schedule', function () {
                     var flag = false;
-                    if($scope.schedule.hour <= 23){
+                    if ($scope.schedule.hour <= 23) {
                         flag = true;
-                    }else{
+                        $scope.schedule.hour = zeroFill($scope.schedule.hour);
+                    } else {
                         flag = false;
                         $scope.schedule.hour = 23;
                     }
-                    
+
                     if ($scope.schedule.minute <= 59) {
                         flag = true;
-                    }else{
+                        $scope.schedule.minute = zeroFill($scope.schedule.minute);
+                    } else {
                         flag = false;
                         $scope.schedule.minute = 59;
                     }
-                    
-                    if($scope.schedule.hour == '' || $scope.schedule.minute == ''){
+
+                    if ($scope.schedule.hour == '' || $scope.schedule.minute == '') {
                         flag = false;
                     }
-                    
-                    if(flag){
+
+                    if (flag) {
                         $scope.scheduledDelivery.date =
                             setTime($scope.scheduledDelivery.date, $scope.schedule.hour, $scope.schedule.minute);
                         $scope.scheduledDelivery.disabled = false;
-                    }else{
+                    } else {
                         $scope.scheduledDelivery.disabled = true;
                     }
-                    
+
                 });
-                
-                function setTime (date, hour, minutes) {
+
+                function setTime(date, hour, minutes) {
                     date.setHours(hour);
                     date.setMinutes(minutes);
                     return date;
