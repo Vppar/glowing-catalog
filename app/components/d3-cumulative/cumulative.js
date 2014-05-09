1f1529eef0c9568f1601d39a3af82caf67a65d31
Mathias Kretschek <mathias@kretschek.com.br>
2014-05-09 17:19:43 -0300

    Histogram chart is redrawn whenever data changes

--- app/components/d3-cumulative/cumulative.js
+++ app/components/d3-cumulative/cumulative.js
@@ -18,123 +18,143 @@
                 values : '=values'
             },
             link: function (scope, element, attrs) {
+                //get the json with data
+                var values = null;
 
-               //get the json with data
-               var values = scope.values;
-
-               //get the values that should be displayed
-               var bands = scope.values.bands;
-
-
-               var width = 800; 
-               var height = 200;
-
-               var leftMargin = 40;
-               var bottomMargin = 40;
-               var topMargin = 10;
-
-               //get the max value of goals (this is needed to make the chart y scale) 
-               var max = d3.max(bands, function(d){ return d.goal});
-
-               //calcs the real chart width
-               var chartWidth = width-leftMargin;
-
-               //calcs the real chart height
-               var chartHeight = height-(topMargin+bottomMargin);
-
-               //creates the x scale for the chart. its an ordinal scale!   
-               var x = d3.scale.ordinal().domain(bands.map(function(d){ return d.order;})).rangeRoundBands([leftMargin,width],0);
-               
-               //creates the x scale for the chart. this is a linear scale!
-               var y = d3.scale.linear().domain([max,0]).range([0,chartHeight]);
-
-               //creates the canvas tha will hold all
-               var canvas = d3.select('body').selectAll('div.cumulative').append('svg')
-               .attr('width', width)
-               .attr('height', height);
-
-             //this is the right way!!
-             var goals = bands.map(function(d){ return d.goal;}).filter(Number);
-             var snapshots = bands.map(function(d){ return d.snapshot;}).filter(Number);
-
-             var byOrder = d3.nest().key(function(d){ return 'o'+d.order;}).map(bands,d3.map);
+                //get the values that should be displayed
+                var bands = null;
 
 
-             drawXAxis();
-             drawYAxis();
-             drawGuide(2);
-             drawLine('#5892ce',goals);
-             drawLine('#ab147a',snapshots);
+                var width = 800; 
+                var height = 200;
 
-             function drawXAxis(){
-                var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(function(d){ return byOrder.get('o'+d)[0].label; });
+                var leftMargin = 40;
+                var bottomMargin = 40;
+                var topMargin = 10;
 
-                canvas.append("g")
-                .attr('class','axis-x')
-                .attr("transform", "translate(0,"+(chartHeight+topMargin)+")")
-                .call(xAxis);
+                //get the max value of goals (this is needed to make the chart y scale) 
+                var max = null;
 
-            }
+                //calcs the real chart width
+                var chartWidth = null;
 
-            function drawGuide(order){
+                //calcs the real chart height
+                var chartHeight = null;
 
-                var guide = canvas.append("line")
-                .attr('class','guide')
-                .attr("x1", x(order)+x.rangeBand()/2)
-                .attr("x2", x(order)+x.rangeBand()/2)
-                .attr("y1", function(d){ return 0;})
-                .attr("y2", function(d){ return chartHeight+topMargin;});
+                //creates the x scale for the chart. its an ordinal scale!   
+                var x = null;
+                 
+                //creates the x scale for the chart. this is a linear scale!
+                var y = null;
 
-            }
+                //creates the canvas tha will hold all
+                var canvas = d3.select(element[0]).append('svg');
 
-            function drawLine(color,points){
-                var lineMap = d3.svg.line()
-                .x(function(d,i) { 
-                    return ((i) * x.rangeBand()) + leftMargin + x.rangeBand()/2; 
-                })
-                .y(function(d) { 
-                    return y(d);
-                })
+                var goals = null;
+                var snapshots = null;
+                var byOrder = null;
 
-                var line = canvas.append('path')
-                .attr("d", lineMap(points))
-                .attr('stroke',color)
-                .attr('fill','none')
-                .attr('stroke-linejoin','round')
-                .attr('stroke-width','2px')
-                .attr("transform", "translate(0,"+topMargin+")");
+                function clear() {
+                    canvas.selectAll('*').remove();
+                }
 
 
-            }
+                function setValues() {
+                    values = scope.values;
+                    bands = scope.values.bands;
+                    max = d3.max(bands, function(d){ return d.goal});
+                    chartWidth = width-leftMargin;
+                    chartHeight = height-(topMargin+bottomMargin);
+                    x = d3.scale.ordinal().domain(bands.map(function(d){ return d.order;})).rangeRoundBands([leftMargin,width],0);
+                    y = d3.scale.linear().domain([max,0]).range([0,chartHeight]);
+                    goals = bands.map(function(d){ return d.goal;}).filter(Number);
+                    snapshots = bands.map(function(d){ return d.snapshot;}).filter(Number);
+                    byOrder = d3.nest().key(function(d){ return 'o'+d.order;}).map(bands,d3.map);
+                }
 
-            function drawYAxis(){
 
-                var qty = 6;
+                function draw() {
+                    clear();
+                    setValues();
 
-                var yAxis =
-                d3.svg.axis()
-                .scale(y)
-                .ticks(qty)
-                .orient('left');
+                    canvas
+                        .attr('width', width)
+                        .attr('height', height);
+                        
+                    drawXAxis();
+                    drawYAxis();
+                    drawGuide(2);
+                    drawLine('#5892ce',goals);
+                    drawLine('#ab147a',snapshots);
+                }
 
-                var axisY = canvas.append("g")
-                .attr('class','axis-y')
-                .attr("transform", "translate("+leftMargin+","+topMargin+")")
-                .call(yAxis);
 
-                axisY.selectAll("line.y")
-                .data(y.ticks(qty))
-                .enter().append("line")
-                .attr('class','guide')
-                .attr("x1", 0)
-                .attr("x2", leftMargin + chartWidth)
-                .attr("y1", function(d){ return y(d);})
-                .attr("y2", function(d){ return y(d);});
-            }       
+                function drawXAxis(){
+                    var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(function(d){
+                        return byOrder.get('o'+d)[0].label;
+                    });
 
-            
+                    canvas.append("g")
+                        .attr('class','axis-x')
+                        .attr("transform", "translate(0,"+(chartHeight+topMargin)+")")
+                        .call(xAxis);
+                }
 
-        }
-    }
+
+                function drawGuide(order){
+                    var guide = canvas.append("line")
+                        .attr('class','guide')
+                        .attr("x1", x(order)+x.rangeBand()/2)
+                        .attr("x2", x(order)+x.rangeBand()/2)
+                        .attr("y1", function(d){ return 0;})
+                        .attr("y2", function(d){ return chartHeight+topMargin;});
+                }
+
+
+                function drawLine(color,points){
+                    var lineMap = d3.svg.line()
+                        .x(function(d,i) { 
+                            return ((i) * x.rangeBand()) + leftMargin + x.rangeBand()/2; 
+                        })
+                        .y(function(d) { 
+                            return y(d);
+                        })
+
+                    var line = canvas.append('path')
+                        .attr("d", lineMap(points))
+                        .attr('stroke',color)
+                        .attr('fill','none')
+                        .attr('stroke-linejoin','round')
+                        .attr('stroke-width','2px')
+                        .attr("transform", "translate(0,"+topMargin+")");
+                }
+
+
+                function drawYAxis(){
+                    var qty = 6;
+                    var yAxis = d3.svg.axis()
+                        .scale(y)
+                        .ticks(qty)
+                        .orient('left');
+
+                    var axisY = canvas.append("g")
+                        .attr('class','axis-y')
+                        .attr("transform", "translate("+leftMargin+","+topMargin+")")
+                        .call(yAxis);
+
+                    axisY.selectAll("line.y")
+                        .data(y.ticks(qty))
+                        .enter().append("line")
+                        .attr('class','guide')
+                        .attr("x1", 0)
+                        .attr("x2", leftMargin + chartWidth)
+                        .attr("y1", function(d){ return y(d);})
+                        .attr("y2", function(d){ return y(d);});
+                }       
+
+
+                scope.$watchCollection('values', draw);
+          }
+      }
 });
 }(angular));
