272a0966e6fdf835404b4da0c759c4c1e0f657ef
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2014-05-11 17:13:48 -0300

    Adding menu item, blocking undesired access and hiding tabs.

--- app/scripts/controllers/bi.js
+++ app/scripts/controllers/bi.js
@@ -1,62 +1,66 @@
-'use strict';
+(function(angular) {
+    'use strict';
 
-angular
-    .module('tnt.catalog.bi.ctrl', [
-      'tnt.catalog.sync.driver'
-    ])
-    .controller('BiCtrl', ['$scope', '$log', 'SyncDriver', function ($scope, $log, SyncDriver) {
+    angular.module('tnt.catalog.bi.ctrl', [
+        'tnt.catalog.sync.driver'
+    ]).controller('BiCtrl', [
+        '$scope', '$log', 'SyncDriver', 'UserService', function($scope, $log, SyncDriver, UserService) {
 
-      $log.info('Initializing BICtrl...');
+            UserService.redirectIfIsNotLoggedIn();
 
-      var biRef = SyncDriver.refs.bi;
+            $log.info('Initializing BICtrl...');
 
-      var nullData = {
-        gauge : {
-          goal : 1,
-          snapshot : 0,
-          percent : 0
-        },
+            var biRef = SyncDriver.refs.bi;
 
-        histogram : {
-          bands : []
-        },
+            var nullData = {
+                gauge : {
+                    goal : 1,
+                    snapshot : 0,
+                    percent : 0
+                },
 
-        termometer : {
-          goal : 1,
-          snapshot : 0,
-          percent : 0
-        },
+                histogram : {
+                    bands : []
+                },
 
-        cumulative : {
-          bands : []
+                termometer : {
+                    goal : 1,
+                    snapshot : 0,
+                    percent : 0
+                },
+
+                cumulative : {
+                    bands : []
+                }
+            };
+
+            var data = {};
+
+            // Set some default values to prevent things to get messy if there's
+            // no
+            // BI data for the given user.
+            angular.extend(data, nullData);
+
+            // This 'if' avoids the need to signin before every test (should
+            // happen only
+            // during development)
+            if (biRef) {
+                biRef.on('value', function(snapshot) {
+                    if (snapshot) {
+                        var val = snapshot.val();
+
+                        $log.debug('BI data received...', val);
+
+                        data.gauge = val.gauge || nullData.gauge;
+                        data.histogram = val.histogram || nullData.histogram;
+                        data.termometer = val.termometer || nullData.termometer;
+                        data.cumulative = val.cumulative || nullData.cumulative;
+                    }
+                });
+            }
+
+            // Expose the data to the view
+            $scope.data = data;
         }
-      };
-
-      var data = {};
-
-      // Set some default values to prevent things to get messy if there's no
-      // BI data for the given user.
-      angular.extend(data, nullData);
-
-
-      // This 'if' avoids the need to signin before every test (should happen only
-      // during development)
-      if (biRef) {
-        biRef.on('value', function (snapshot) {
-          if (snapshot) {
-            var val = snapshot.val();
-
-            $log.debug('BI data received...', val);
-
-            data.gauge = val.gauge || nullData.gauge;
-            data.histogram = val.histogram || nullData.histogram;
-            data.termometer = val.termometer || nullData.termometer;
-            data.cumulative = val.cumulative || nullData.cumulative;
-          }
-        });
-      }
-
-
-      // Expose the data to the view
-      $scope.data = data;
-    }]);
+    ]);
+})(angular);
