5a0c3ce6afbe133f71c2cb2f2ac9f0a68bdd7720
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2014-08-27 11:21:34 -0300

    Fixing catalog scroll.

--- app/scripts/directives/spy.js
+++ app/scripts/directives/spy.js
@@ -1,42 +1,38 @@
-(function(angular) {
-    'use strict';
+(function (angular) {
+  'use strict';
 
-    angular.module('glowingCatalogApp').directive('spy', function() {
-        return {
-            restrict : 'A',
-            require : '^scrollSpy',
-            link : function(scope, elem, attrs, ctrl) {
+  angular.module('glowingCatalogApp').directive('spy', function () {
+    return {
+      restrict: 'A',
+      require: '^scrollSpy',
+      link: function (scope, elem, attrs, ctrl) {
 
-                var spyObj = {};
+        var spyObj = {};
 
-                attrs.$observe('spy', function(val) {
+        attrs.$observe('spy', function (val) {
 
-                    spyObj = {
-                        id : val,
-                        'in' : function() {
-                            return elem.addClass('active');
-                        },
-                        out : function() {
-                            return elem.removeClass('active');
-                        }
-                    };
-
-                    ctrl.addSpy(spyObj);
-
-                    if (val == 'cat0') {
-                        elem.addClass('active');
-                    }
-                });
-
-                scope.$on('$destroy', function() {
-                    ctrl.delSpy(spyObj);
-                });
-
-                elem.bind('click', function() {
-                    ctrl.scroll(attrs.spy);
-                    scope.$apply();
-                });
+          spyObj = {
+            id: val,
+            'in': function () {
+              return scope.$apply(attrs.spyScrollIn);
+            },
+            out: function () {
+              return angular.noop;
             }
-        };
-    });
+          };
+
+          ctrl.addSpy(spyObj);
+        });
+
+        scope.$on('$destroy', function () {
+          ctrl.delSpy(spyObj);
+        });
+
+        elem.bind('click', function () {
+          ctrl.scroll(attrs.spy);
+          scope.$apply();
+        });
+      }
+    };
+  });
 })(angular);
